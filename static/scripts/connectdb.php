5e5c2f430301f66d1821aeb280f06a0aace43531
padillac <shinimaninima@gmail.com>
2018-11-29 14:40:52 -0800

    changed dbaccount file to be secure for apache (unable to be served to web users)

--- static/scripts/connectdb.php
+++ static/scripts/connectdb.php
@@ -1,6 +1,6 @@
 <?php
 
   echo "<h1>HELLO PHP</h1>";
-  include $_SERVER['DOCUMENT_ROOT']."/static/dbinfo/dbaccount.php";
+  include $_SERVER['DOCUMENT_ROOT']."/static/dbinfo/.htdbaccount.php";
   /*$db_connection = pg_connect();*/
 ?>

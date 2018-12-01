e8218cf1a59337dfb43eb94513fe6a3f6825cadd
padillac <shinimaninima@gmail.com>
2018-11-30 21:08:39 -0800

    added new php pages for all admin create-object commands

--- admin/create-new-object/new-client/index.php
+++ admin/create-new-object/new-client/index.php
@@ -5,27 +5,19 @@
   <meta charset="utf-8">
   <link rel="stylesheet" href="/static/main.css">
   <?php include $_SERVER['DOCUMENT_ROOT'] . "/static/scripts/connectdb.php";?>
-  <title>Admin | Create Object</title>
+  <title>Admin | New Client</title>
 </head>
 
 <body>
 
   <header>
-    <h1>Create Object</h1>
+    <h1>New Client</h1>
     <nav>
       <a href="/"><button id="home-button">Home</button></a>
     </nav>
   </header>
 
-  <h3 class="main-content-header">Select type of object:</h3>
 
-  <div class="main-content-div">
-    <a href=""><button>Worker</button></a>
-    <a href=""><button>Client</button></a>
-    <a href=""><button>Horse</button></a>
-    <a href=""><button>Other</button></a>
-
-  </div>
 
 
 

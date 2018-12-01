5eebb6ddd170e89df67d92c21ad7e8dc3fc8fecc
padillac <shinimaninima@gmail.com>
2018-11-30 20:49:13 -0800

    added create object button and page to admin

---
+++ admin/create-new-object/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <?php include $_SERVER['DOCUMENT_ROOT'] . "/static/scripts/connectdb.php";?>
+  <title>Admin | Create Object</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Create Object</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  
+
+
+
+
+
+</body>
+
+</html>

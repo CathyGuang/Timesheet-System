df4a120cd38c9e0d41dff4cf96b8e590192f4ae2
padillac <shinimaninima@gmail.com>
2018-11-30 22:21:46 -0800

    finished add client functionality!

--- admin/create-new-object/new-client/create-new-client.php
+++ admin/create-new-object/new-client/create-new-client.php
@@ -19,9 +19,15 @@
 
 
   <?php
-    print_r($_POST);
+    $query = "INSERT INTO clients(name, email, phone) VALUES ('{$_POST['name']}', '{$_POST['email']}', '{$_POST['phone']}');";
 
+    $result = pg_query($db_connection, $query);
 
+    if ($result) {
+      echo "<h3 class='main-content-header'>Success</h3";
+    } else {
+      echo "<h3 class='main-content-header>An error occured.</h3><p class='main-content-header'>Please try again, ensure that all data is correctly formatted.</p>";
+    }
   ?>
 
 

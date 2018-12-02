05930f180373345da693e1fe620b10b9bc49a2a3
padillac <shinimaninima@gmail.com>
2018-12-01 17:42:06 -0800

    added blank pages for all admin object edits

---
+++ admin/edit-object/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <title>Admin | Edit Object</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Edit Object</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  <h3 class="main-content-header">Select type of object to edit:</h3>
+
+  <div class="main-content-div">
+    <a href="edit-worker"><button>Worker</button></a>
+    <a href="edit-client"><button>Client</button></a>
+    <a href="edit-horse"><button>Horse</button></a>
+    <a href="edit-other"><button>Other</button></a>
+
+  </div>
+
+
+
+
+
+</body>
+
+</html>

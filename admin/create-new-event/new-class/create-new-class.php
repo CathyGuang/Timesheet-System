0ee75a5b0e6b8af94c42ac00e97b4d87e12378f3
padillac <shinimaninima@gmail.com>
2018-12-02 22:15:02 -0800

    finished add-class pages, and added SQL create scripts to initialize necessary none values

--- admin/create-new-event/new-class/create-new-class.php
+++ admin/create-new-event/new-class/create-new-class.php
@@ -19,25 +19,83 @@
 
   <?php
     //Process form input
-    var_dump($_POST);
     //get array of dates and times
     $date = $_POST['start-date'];
     $end_date = $_POST['end-date'];
+    $dateTimeTriplets = array();
     while (strtotime($date) <= strtotime($end_date)) {
-      $dayOfWeek = date('w', strtotime($date));
-      echo $dayOfWeek;
+      $dayOfWeek = date('l', strtotime($date));
+      if (in_array($dayOfWeek, $_POST)) {
+        $startTime =  $_POST[strtolower($dayOfWeek).'-start-time'];
+        $endTime = $_POST[strtolower($dayOfWeek).'-end-time'];
+        $dateTimeTriplets[$date] = array($startTime, $endTime);
+      }
 
+      //looper
       $date = date ('Y-m-d', strtotime('+1 day', strtotime($date)));
     }
+    //Convert other user selections to database ids
 
+    function to_pg_array($set) {
+      settype($set, 'array'); // can be called with a scalar or array
+      $result = array();
+      foreach ($set as $t) {
+          if (is_array($t)) {
+              $result[] = to_pg_array($t);
+          } else {
+              $t = str_replace('"', '\\"', $t); // escape double quote
+              if (! is_numeric($t)) // quote only non-numeric values
+                  $t = '"' . $t . '"';
+              $result[] = $t;
+          }
+      }
+      return '{' . implode(",", $result) . '}'; // format
+    }
 
+    $horseID = pg_fetch_row(pg_query($db_connection, "SELECT id FROM horses WHERE name LIKE '{$_POST['horse']}'"))[0];
+    if (!$horseID) {
+      $horseID = 'null';
+    }
+    $clientIDList = array();
+    foreach ($_POST['clients'] as $key => $value) {
+      $id = pg_fetch_row(pg_query($db_connection, "SELECT id FROM clients WHERE name LIKE '{$value}'"))[0];
+      $clientIDList[] = $id;
+    }
+    $clientIDList = to_pg_array($clientIDList);
+
+    $instructorID = pg_fetch_row(pg_query($db_connection, "SELECT id FROM workers WHERE name LIKE '{$_POST['instructor']}'"))[0];
+    if (!$instructorID) {
+      $instructorID = 'null';
+    }
+    $therapistID = pg_fetch_row(pg_query($db_connection, "SELECT id FROM workers WHERE name LIKE '{$_POST['therapist']}'"))[0];
+    if (!$therapistID) {
+      $therapistID = 'null';
+    }
+    $esID = pg_fetch_row(pg_query($db_connection, "SELECT id FROM workers WHERE name LIKE '{$_POST['equine-specialist']}'"))[0];
+    if (!$esID) {
+      $esID = 'null';
+    }
+    $leaderID = pg_fetch_row(pg_query($db_connection, "SELECT id FROM workers WHERE name LIKE '{$_POST['leader']}'"))[0];
+    if (!$leaderID) {
+      $leaderID = 'null';
+    }
+    $sidewalkerIDList = array();
+    foreach ($_POST['sidewalkers'] as $key => $value) {
+      $id = pg_fetch_row(pg_query($db_connection, "SELECT id FROM workers WHERE name LIKE '{$value}'"))[0];
+      $sidewalkerIDList[] = $id;
+    }
+    $sidewalkerIDList = to_pg_array($sidewalkerIDList);
 
 
     //Create SQL query
-    $query = <<<EOT
-    INSERT INTO classes (class_type, date_of_class, time_of_class, arena, horse, tack, special_tack, stirrup_leather_length, pad, clients, instructor, therapist, equine_specialist, leader, sidewalkers)
-    VALUES ('{$_POST['class-type']}', {$specificDate}, {$specificTime}, '{$_POST['arena']}', {$horseID}, '{$_POST['tack']}', '{$_POST['special-tack']}', '{$_POST['stirrup-leather-length']}', '{$_POST['pad']}', {$clientIDList}, {$instructorID}, {$therapistID}, {$esID}, {$leaderID}, {$sidewalkerIDList});
-EOT;
+    $query = "INSERT INTO classes (class_type, date_of_class, start_time, end_time, arena, horse, tack, special_tack, stirrup_leather_length, pad, clients, instructor, therapist, equine_specialist, leader, sidewalkers) VALUES";
+    foreach ($dateTimeTriplets as $date => $timeArray) {
+      $query = $query . "('{$_POST['class-type']}', '{$date}', '{$timeArray[0]}', '{$timeArray[1]}', '{$_POST['arena']}', {$horseID}, '{$_POST['tack']}', '{$_POST['special-tack']}', '{$_POST['stirrup-leather-length']}', '{$_POST['pad']}', '{$clientIDList}', {$instructorID}, {$therapistID}, {$esID}, {$leaderID}, '{$sidewalkerIDList}'),";
+    }
+
+    $query = chop($query, ",") . ";";
+
+
 
     //Modify database
     $result = pg_query($db_connection, $query);
--- admin/create-new-event/new-class/index.php
+++ admin/create-new-event/new-class/index.php
@@ -44,49 +44,49 @@
     <div style="max-width: 420px;">
       <!-- MONDAY-->
       <label for="monday-checkbox">Monday: </label>
-      <input type="checkbox" id="monday-checkbox" name="monday-checkbox">
+      <input type="checkbox" id="monday-checkbox" name="monday-checkbox" value="Monday">
       <label for="monday-start-time">from:</label>
       <input type="time" id="monday-start-time" name="monday-start-time">
       <label for="monday-end-time">to:</label>
       <input type="time" id="monday-end-time" name="monday-end-time">
       <!-- TUESDAY-->
       <label for="tuesday-checkbox">Tuesday: </label>
-      <input type="checkbox" id="tuesday-checkbox" name="tuesday-checkbox">
+      <input type="checkbox" id="tuesday-checkbox" name="tuesday-checkbox" value="Tuesday">
       <label for="tuesday-start-time">from:</label>
       <input type="time" id="tuesday-start-time" name="tuesday-start-time">
       <label for="tuesday-end-time">to:</label>
       <input type="time" id="tuesday-end-time" name="tuesday-end-time">
       <!-- WEDNESDAY-->
       <label for="wednesday-checkbox">Wednesday: </label>
-      <input type="checkbox" id="wednesday-checkbox" name="wednesday-checkbox">
+      <input type="checkbox" id="wednesday-checkbox" name="wednesday-checkbox" value="Wednesday">
       <label for="wednesday-start-time">from:</label>
       <input type="time" id="wednesday-start-time" name="wednesday-start-time">
       <label for="wednesday-end-time">to:</label>
       <input type="time" id="wednesday-end-time" name="wednesday-end-time">
       <!-- THURSDAY-->
       <label for="thursday-checkbox">Thursday: </label>
-      <input type="checkbox" id="thursday-checkbox" name="thursday-checkbox">
+      <input type="checkbox" id="thursday-checkbox" name="thursday-checkbox" value="Thursday">
       <label for="thursday-start-time">from:</label>
       <input type="time" id="thursday-start-time" name="thursday-start-time">
       <label for="thursday-end-time">to:</label>
       <input type="time" id="thursday-end-time" name="thursday-end-time">
       <!-- FRIDAY-->
       <label for="friday-checkbox">Friday: </label>
-      <input type="checkbox" id="friday-checkbox" name="friday-checkbox">
+      <input type="checkbox" id="friday-checkbox" name="friday-checkbox" value="Friday">
       <label for="friday-start-time">from:</label>
       <input type="time" id="friday-start-time" name="friday-start-time">
       <label for="friday-end-time">to:</label>
       <input type="time" id="friday-end-time" name="friday-end-time">
       <!-- SATURDAY-->
       <label for="saturday-checkbox">Saturday: </label>
-      <input type="checkbox" id="saturday-checkbox" name="saturday-checkbox">
+      <input type="checkbox" id="saturday-checkbox" name="saturday-checkbox" value="Saturday">
       <label for="saturday-start-time">from:</label>
       <input type="time" id="saturday-start-time" name="saturday-start-time">
       <label for="saturday-end-time">to:</label>
       <input type="time" id="saturday-end-time" name="saturday-end-time">
       <!-- SUNDAY-->
       <label for="sunday-checkbox">Sunday: </label>
-      <input type="checkbox" id="sunday-checkbox" name="sunday-checkbox">
+      <input type="checkbox" id="sunday-checkbox" name="sunday-checkbox" value="Sunday">
       <label for="sunday-start-time">from:</label>
       <input type="time" id="sunday-start-time" name="sunday-start-time">
       <label for="sunday-end-time">to:</label>
@@ -94,7 +94,7 @@
     </div>
 
     <p>Arena:</p>
-    <input type="text" name="arena" list="arena-list">
+    <input type="text" name="arena" list="arena-list" value="none" onclick="select();">
       <datalist id="arena-list">
         <?php
           $query = "SELECT unnest(enum_range(NULL::ARENA))";
@@ -107,7 +107,7 @@
       </datalist>
 
     <p>Horse:</p>
-    <input type="text" name="horse" list="horse-list">
+    <input type="text" name="horse" list="horse-list" value="none" onclick="select();">
       <datalist id="horse-list">
         <?php
           $query = "SELECT name FROM horses;";
@@ -120,7 +120,7 @@
       </datalist>
 
     <p>Tack:</p>
-    <input type="text" name="tack" list="tack-list">
+    <input type="text" name="tack" list="tack-list" value="none" onclick="select();">
       <datalist id="tack-list">
         <?php
           $query = "SELECT unnest(enum_range(NULL::TACK))";
@@ -139,7 +139,7 @@
     <input type="text" name="stirrup-leather-length">
 
     <p>Pad:</p>
-    <input type="text" name="pad" list="pad-list">
+    <input type="text" name="pad" list="pad-list" value="none" onclick="select();">
       <datalist id="pad-list">
         <?php
           $query = "SELECT unnest(enum_range(NULL::PAD))";
@@ -152,7 +152,7 @@
       </datalist>
     <div id="client-section">
       <p>Clients:</p>
-      <input type="text" name="client" list="client-list">
+      <input type="text" name="clients[]" list="client-list" value="none" onclick="select();">
         <datalist id="client-list">
           <?php
             $query = "SELECT name FROM clients;";
@@ -168,7 +168,7 @@
     <button type="button" id="add-client-button" onclick="newClientFunction();">Add Client</button>
 
     <p>Instructor:</p>
-    <input type="text" name="instructor" list="instructor-list">
+    <input type="text" name="instructor" list="instructor-list" value="none" onclick="select();">
       <datalist id="instructor-list">
         <?php
           $query = "SELECT name FROM workers;";
@@ -181,7 +181,7 @@
       </datalist>
 
     <p>Therapist:</p>
-    <input type="text" name="therapist" list="therapist-list">
+    <input type="text" name="therapist" list="therapist-list" value="none" onclick="select();">
       <datalist id="therapist-list">
         <?php
           $query = "SELECT name FROM workers;";
@@ -194,7 +194,7 @@
       </datalist>
 
     <p>ES:</p>
-    <input type="text" name="equine-specialist" list="es-list">
+    <input type="text" name="equine-specialist" list="es-list" value="none" onclick="select();">
       <datalist id="es-list">
         <?php
           $query = "SELECT name FROM workers;";
@@ -207,7 +207,7 @@
       </datalist>
 
     <p>Leader:</p>
-    <input type="text" name="leader" list="leader-list">
+    <input type="text" name="leader" list="leader-list" value="none" onclick="select();">
       <datalist id="leader-list">
         <?php
           $query = "SELECT name FROM workers;";
@@ -221,7 +221,7 @@
 
       <div id="sidewalker-section">
         <p>Sidewalkers:</p>
-        <input type="text" name="sidewalker" list="sidewalker-list">
+        <input type="text" name="sidewalkers[]" list="sidewalker-list" value="none" onclick="select();">
           <datalist id="sidewalker-list">
             <?php
               $query = "SELECT name FROM workers;";
@@ -250,8 +250,10 @@
     function newClientFunction() {
         newInput = document.createElement('input');
         newInput.setAttribute('type', 'text');
-        newInput.setAttribute('name', 'client');
+        newInput.setAttribute('name', 'clients[]');
         newInput.setAttribute('list', 'client-list');
+        newInput.setAttribute('value', 'none');
+        newInput.setAttribute('onclick', 'select()');
         var clientSection = document.getElementById('client-section');
         clientSection.appendChild(newInput);
       };
@@ -259,8 +261,10 @@
     function newSidewalkerFunction() {
       newInput = document.createElement('input');
       newInput.setAttribute('type', 'text');
-      newInput.setAttribute('name', 'sidewalker');
+      newInput.setAttribute('name', 'sidewalkers[]');
       newInput.setAttribute('list', 'sidewalker-list');
+      newInput.setAttribute('value', 'none');
+      newInput.setAttribute('onclick', 'select()');
       var sidewalkerSection = document.getElementById('sidewalker-section');
       sidewalkerSection.appendChild(newInput);
     };

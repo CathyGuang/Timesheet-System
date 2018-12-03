6d7f990d5045031c5da34ee685ff2bd9d11461b9
padillac <shinimaninima@gmail.com>
2018-12-03 15:32:54 -0800

    added folders and php pages for editing volunteer shifts

---
+++ admin/edit-event/edit-volunteer-shift/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <title>Admin | Edit Volunteer Shift</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Edit Volunteer Shift</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  <h3 class="main-content-header">Select type of shift to edit</h3>
+
+  <div class="main-content-div">
+    <a href="edit-horse-care-shift"><button>Horse Care Shift</button></a>
+    <a href="edit-office-shift"><button>Office Shift</button></a>
+
+  </div>
+
+
+
+
+
+</body>
+
+</html>

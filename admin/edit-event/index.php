de557fd2de4d47ebf9b730177aa417b88fc5bacf
padillac <shinimaninima@gmail.com>
2018-12-03 09:12:16 -0800

    added blank pages for editing events

---
+++ admin/edit-event/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <title>Admin | Create Event</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Create Event</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  <h3 class="main-content-header">Select type of event to edit:</h3>
+
+  <div class="main-content-div">
+    <a href="edit-class"><button>Class</button></a>
+    <a href="edit-volunteer-shift"><button>Volunteer Shift</button></a>
+
+  </div>
+
+
+
+
+
+</body>
+
+</html>

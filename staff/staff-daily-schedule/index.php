8502abce430412bca632d4d9aadc2201c7fba13c
padillac <shinimaninima@gmail.com>
2018-12-05 14:34:46 -0800

    added staff daily schedule functionality

---
+++ staff/staff-daily-schedule/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <?php include $_SERVER['DOCUMENT_ROOT'] . "/static/scripts/connectdb.php";?>
+  <title>Staff Daily Schedule</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Staff Daily Schedule</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  <div class="main-content-div">
+
+    <form action="schedule.php" method="post" class="main-form">
+      <p>Select your name:</p>
+      <input name="selected-name" list="staff">
+      <datalist id="staff">
+        <?php
+          $staffNames = pg_fetch_all_columns(pg_query($db_connection, "SELECT name FROM workers WHERE staff = TRUE;"));
+          foreach ($staffNames as $name) {
+            echo "<option value='$name'>";
+          }
+        ?>
+      </datalist>
+      <input type="date" name="selected-date" value="<?php echo date('Y-m-d') ?>">
+
+      <input type="submit" value="Search">
+    </form>
+
+  </div>
+
+
+</body>
+
+</html>
---
+++ staff/staff-daily-schedule/schedule.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/connectdb.php"; ?>
+  <?php error_reporting(E_ALL & ~E_NOTICE); ?>
+  <title>Staff Daily Schedule</title>
+</head>
+
+<body>
+
+  <header>
+    <h1><?php echo $_POST['selected-name'] ?>'s Daily Schedule</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  <h2 class="main-content-header"><?php if ($_POST['selected-date'] == date('Y-m-d')) {echo "TODAY";} else {echo "For {$_POST['selected-date']}";} ?></h2>
+
+  <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/scheduleGenerator.php"; ?>
+
+
+</body>
+
+</html>

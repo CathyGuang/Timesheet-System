ce551af85d350abdc3d141d97a8da119421b26ae
padillac <shinimaninima@gmail.com>
2018-12-05 13:57:52 -0800

    finished displaying schedule grid

--- volunteer/volunteer-daily-schedule/schedule.php
+++ volunteer/volunteer-daily-schedule/schedule.php
@@ -108,39 +108,43 @@ EOT;
 
     foreach ($masterList as $time => $event) {
 
+      //Time
       $newTimeString = date("g:i a", strtotime($time)) . "<br> &#8212 <br>" . date("g:i a", strtotime($event['end_time']));
       echo "<p class='schedule-time'>{$newTimeString}</p>";
 
+      //Event Type
       echo "<p class='schedule-event-type'>{$event['class_type']}{$event['care_type']}{$event['shift_type']}</p>";
 
+      //Staff
       $staffString = "";
       if ($event['instructor']) {
         $staffString .= "<i>Instructor: </i>" . $event['instructor'];
       }
       if ($event['therapist'] != "none" and $event['therapist']) {
-        $staffString .= "<i> Therapist: </i>" . $event['therapist'];
+        $staffString .= "<br><i>Therapist: </i>" . $event['therapist'];
       }
       if ($event['equine_specialist'] != "none" and $event['equine_specialist']) {
-        $staffString .= "<i> ES: </i>" . $event['equine_specialist'];
+        $staffString .= "<br><i>ES: </i>" . $event['equine_specialist'];
       }
       if ($staffString == "") {
         $staffString = "&#8212";
       }
       echo "<p class='schedule-staff'>{$staffString}</p>";
 
+      //Volunteers
       $volunteerString = "";
       if ($event['leader'] != "none") {
         $volunteerString .= "<i>Leader: </i>" . $event['leader'];
       }
       if ($event['volunteers']) {
         foreach ($event['volunteers'] as $volunteerName) {
-          $volunteerString .= "<i> Volunteer: </i>" . $volunteerName;
+          $volunteerString .= "<br><i>Volunteer: </i>" . $volunteerName;
         }
       }
       if ($event['sidewalkers']) {
         foreach ($event['sidewalkers'] as $volunteerName) {
           if ($volunteerName != "none") {
-            $volunteerString .= "<i> Sidewalker: </i>" . $volunteerName;
+            $volunteerString .= "<br><i>Sidewalker: </i>" . $volunteerName;
           }
         }
       }
@@ -149,8 +153,43 @@ EOT;
       }
       echo "<p class='schedule-volunteers'>{$volunteerString}</p>";
 
+      //Horse
+      $horseString = "";
+      if ($event['horse'] and $event['horse'] != "none") {
+        $horseString .= "<i>Horse: </i>" . $event['horse'] . ", ";
+      }
+      if ($event['tack'] and $event['tack'] != "none") {
+        $horseString .= "<br><i>Tack: </i>" . $event['tack'] . ", ";
+      }
+      if ($event['special_tack'] and $event['special_tack'] != "none") {
+        $horseString .= "<i><br>Special Tack: </i>" . $event['special_tack'] . ", ";
+      }
+      if ($event['stirrup_leather_length'] and $event['stirrup_leather_length'] != "none") {
+        $horseString .= "<i><br>Stirrup Leather Length: </i>" . $event['stirrup_leather_length'] . ", ";
+      }
+      if ($event['pad'] and $event['pad'] != "none") {
+        $horseString .= "<i><br>Pad: </i>" . $event['pad'];
+      }
+      if ($horseString == "") {
+        $horseString = "&#8212";
+      }
+      echo "<p class='schedule-horse-info'>{$horseString}</p>";
 
+      //Clients
+      $clientString = "";
+      if ($event['clients']) {
+        $clientString = "<i>Clients: </i>";
+        foreach ($event['clients'] as $clientName) {
+          $clientString .= $clientName . ", ";
+        }
+      }
+      if ($clientString == "") {
+        $clientString = "&#8212";
+      }
+      echo "<p class='schedule-clients'>{$clientString}</p>";
 
+      //Lesson Plan
+      echo "<p class='schedule-lesson-plan'>{$event['lesson_plan']}</p>";
 
     }
 

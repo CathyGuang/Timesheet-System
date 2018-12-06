767b31568bd002e5bff75698e493121ae964860b
padillac <shinimaninima@gmail.com>
2018-12-05 16:26:37 -0800

    volunteer record hours is finishedgit status

---
+++ volunteer/record-hours/volunteer-record-hours.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/static/main.css">
+  <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/connectdb.php"; ?>
+  <title>Record Volunteer Hours</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Volunteer Hours</h1>
+    <nav>
+      <a href="/"><button id="home-button">Home</button></a>
+    </nav>
+  </header>
+
+  <?php
+
+    $volunteerID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM workers WHERE name = '{$_POST['volunteer']}'"), 0, 1)['id'];
+
+    $query = <<<EOT
+      INSERT INTO volunteer_hours (volunteer, hours, shift_type, date_of_hours)
+      VALUES ('{$volunteerID}', '{$_POST['hours']}', '{$_POST['shift-type']}', '{$_POST['date-of-hours']}')
+      ;
+EOT;
+
+    $result = pg_query($db_connection, $query);
+    if ($result) {
+      echo "<h3 class='main-content-header'>Success</h3>";
+    } else {
+      echo "<h3 class='main-content-header>An error occured.</h3><p class='main-content-header'>Please try again, ensure that all data is correctly formatted.</p>";
+    }
+
+
+  ?>
+
+
+</body>
+
+</html>

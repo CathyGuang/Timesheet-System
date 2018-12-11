bfaa01803811ff1b94d4bb45d253ce7eaecb9509
padillac <shinimaninima@gmail.com>
2018-12-10 23:31:30 -0800

    fixed getClient and getWorker involved shift scripts to fit new class profile

--- static/scripts/getWorkerInvolvedClasses.php
+++ static/scripts/getWorkerInvolvedClasses.php
@@ -2,12 +2,12 @@
   $queryID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM workers WHERE workers.name = '{$QUERY_NAME}' AND (archived IS NULL OR archived = '');"), 0, 1)['id'];
 
   $query = <<<EOT
-  SELECT class_type, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tack, special_tack, stirrup_leather_length, pad, horse, instructor, therapist, equine_specialist, leader, sidewalkers, clients FROM classes WHERE
+  SELECT class_type, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, instructor, therapist, equine_specialist, leaders, sidewalkers, clients FROM classes WHERE
   (
   instructor = {$queryID} OR
   therapist = {$queryID} OR
   equine_specialist = {$queryID} OR
-  leader = {$queryID} OR
+  {$queryID} = ANY(leaders) OR
   {$queryID} = ANY(sidewalkers)
   ) AND (
   (archived IS NULL OR archived = '')
@@ -36,9 +36,27 @@ EOT;
           ;
 EOT;
       }
+      if ($specificClass['horses']) {
+        $getHorsesQuery = <<<EOT
+          SELECT name FROM horses WHERE
+          id = ANY('{$allClasses[$key]['horses']}') AND
+          (archived IS NULL OR archived = '')
+          ;
+EOT;
+      }
+      if ($specificClass['leaders']) {
+        $getLeadersQuery = <<<EOT
+          SELECT workers.name FROM workers WHERE
+          workers.id = ANY('{$allClasses[$key]['leaders']}') AND
+          (archived IS NULL OR archived = '')
+          ;
+EOT;
+      }
 
     $clients = pg_fetch_all_columns(pg_query($db_connection, $getClientsQuery));
     $sidewalkers = pg_fetch_all_columns(pg_query($db_connection, $getSidewalkersQuery));
+    $horses = pg_fetch_all_columns(pg_query($db_connection, $getHorsesQuery));
+    $leaders = pg_fetch_all_columns(pg_query($db_connection, $getLeadersQuery));
 
     $allClasses[$key]['clients'] = $clients;
     $allClasses[$key]['sidewalkers'] = $sidewalkers;
@@ -46,10 +64,16 @@ EOT;
     $allClasses[$key]['instructor'] = pg_fetch_array(pg_query($db_connection, "SELECT name FROM workers WHERE id = {$allClasses[$key]['instructor']} ;"))['name'];
     $allClasses[$key]['therapist'] = pg_fetch_array(pg_query($db_connection, "SELECT name FROM workers WHERE id = {$allClasses[$key]['therapist']} ;"))['name'];
     $allClasses[$key]['equine_specialist'] = pg_fetch_array(pg_query($db_connection, "SELECT name FROM workers WHERE id = {$allClasses[$key]['equine_specialist']} ;"))['name'];
-    $allClasses[$key]['leader'] = pg_fetch_array(pg_query($db_connection, "SELECT name FROM workers WHERE id = {$allClasses[$key]['leader']} ;"))['name'];
-    $allClasses[$key]['horse'] = pg_fetch_array(pg_query($db_connection, "SELECT name FROM horses WHERE id = {$allClasses[$key]['horse']} ;"))['name'];
+    $allClasses[$key]['leaders'] = $leaders;
+    $allClasses[$key]['horses'] = $horses;
+
+    $allClasses[$key]['tacks'] = explode(',', rtrim(ltrim($allClasses[$key]['tacks'], '{'), '}'));
+    $allClasses[$key]['pads'] = explode(',', rtrim(ltrim($allClasses[$key]['pads'], '{'), '}'));
+
 
 
   }
+
+
 
 ?>

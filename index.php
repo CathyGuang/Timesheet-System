91bbe07abf16544d2d55653183b3311e39a79dbb
padillac <shinimaninima@gmail.com>
2018-12-17 18:51:56 -0800

    fixed current date on home page and full daily schedule

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="/static/main.css">
+  <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/connectdb.php"; ?>
   <title>Home | Forward Stride Web Portal</title>
 </head>
 

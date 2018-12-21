855a84c38435360493e034f96b11a44b511d725e
padillac <shinimaninima@gmail.com>
2018-12-21 14:53:23 -0500

    added initialization.php script to take load all organization specific info from outside the repository

---
+++ static/scripts/initialization.php
@@ -0,0 +1,13 @@
+<?php
+//Get Organization-Specific Variables
+include $_SERVER['DOCUMENT_ROOT'] . "/../Org-Specific-Info/info.php";
+
+//Initialize timezone for all php scripts
+date_default_timezone_set($organizationTimeZone);
+
+//Connect to Database
+$db_connection = pg_connect("host=localhost dbname=$database user=$dbuser password=$dbpassword");
+
+
+
+?>

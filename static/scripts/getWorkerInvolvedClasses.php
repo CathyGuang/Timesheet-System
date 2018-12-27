9db2f39394ac80829bcda86dda0566a05176ed6b
padillac <shinimaninima@gmail.com>
2018-12-27 01:28:13 -0500

    bugfixing schedule generators10

--- static/scripts/getWorkerInvolvedClasses.php
+++ static/scripts/getWorkerInvolvedClasses.php
@@ -1,13 +1,13 @@
 <?php
   if ($QUERY_NAME == "ALL") {
     $query = <<<EOT
-    SELECT class_type, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, staff, leaders, sidewalkers, clients, attendance FROM classes;
+    SELECT DISTINCT class_type, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, staff, leaders, sidewalkers, clients, attendance FROM classes;
 EOT;
   } else {
     $queryID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM workers WHERE workers.name = '{$QUERY_NAME}' AND (archived IS NULL OR archived = '');"), 0, 1)['id'];
 
     $query = <<<EOT
-    SELECT class_type, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, staff, leaders, sidewalkers, clients, attendance
+    SELECT DISTINCT class_type, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, staff, leaders, sidewalkers, clients, attendance
     FROM classes, jsonb_each_text(classes.staff) WHERE
     (
     '{$queryID}' = value OR

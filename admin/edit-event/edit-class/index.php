de5536e25fbcaee3670ea4f0fc7256b66fa28ab8
padillac <shinimaninima@gmail.com>
2019-01-05 22:51:33 -0600

    added display_title functionality to admin edit class picking class page2

--- admin/edit-event/edit-class/index.php
+++ admin/edit-event/edit-class/index.php
@@ -299,6 +299,7 @@ EOT;
 
 EOT;
         $tackList = explode(',', ltrim(rtrim($classData['tacks'], "}"), '{'));
+          var_dump($tackList);
         foreach ($tackList as $name) {
           echo <<<EOT
           <input form='class-form' type="text" name="tacks[]" list="tack-list" value="{$name}" onclick="select();">

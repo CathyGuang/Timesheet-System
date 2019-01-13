43a8afe75221d62ba848707e0349a913d307aae5
padillac <shinimaninima@gmail.com>
2019-01-12 22:29:56 -0600

    updated SQL queries in get involved classes functions to call 'volunteers' column instead of 'leaders' and 'sidewalkers'

--- static/scripts/getClientInvolvedClasses.php
+++ static/scripts/getClientInvolvedClasses.php
@@ -2,7 +2,7 @@
   $queryID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM clients WHERE name = '{$QUERY_NAME}' AND (archived IS NULL OR archived = '');"), 0, 1)['id'];
 
   $query = <<<EOT
-  SELECT class_type, display_title, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, staff, leaders, sidewalkers, clients, attendance FROM classes WHERE
+  SELECT class_type, display_title, classes.id, cancelled, date_of_class, start_time, end_time, lesson_plan, tacks, special_tack, stirrup_leather_length, pads, horses, staff, leaders, volunteers, attendance FROM classes WHERE
   {$queryID} = ANY(clients) AND
   (archived IS NULL OR archived = '')
   ;

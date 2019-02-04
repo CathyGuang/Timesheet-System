5224b176ae148eed097fdc7bea39b317c91f5bf3
padillac <shinimaninima@gmail.com>
2019-02-04 11:13:53 -0600

    bugfixing horse availability checker7

--- static/scripts/checkAvailability.php
+++ static/scripts/checkAvailability.php
@@ -169,11 +169,13 @@ EOT;
           $horseInfo = pg_fetch_all(pg_query($db_connection, "SELECT * FROM horses WHERE id = {$id} AND (archived IS NULL OR archived = '');"))[0];
 
           foreach ($allEvents as $eventInfo) {
-            $clientNames = pg_fetch_all_columns(pg_query($db_connection, "SELECT name FROM clients WHERE id = ANY('{$eventInfo['clients']}');"));
-            if (in_array($horseInfo['owner'], $clientNames)) {
-              $ownerUseCount++;
-            } else {
-              $orgUseCount++;
+            if ($eventInfo['clients']) {
+              $clientNames = pg_fetch_all_columns(pg_query($db_connection, "SELECT name FROM clients WHERE id = ANY('{$eventInfo['clients']}');"));
+              if (in_array($horseInfo['owner'], $clientNames)) {
+                $ownerUseCount++;
+              } else {
+                $orgUseCount++;
+              }
             }
           }
 

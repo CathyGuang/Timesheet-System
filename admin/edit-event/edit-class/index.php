d82b9b2b90bf4087b87c3e6fefe21f162a4ee095
padillac <shinimaninima@gmail.com>
2019-07-08 12:57:09 -0500

    got rid of bugfixing prints

--- admin/edit-event/edit-class/index.php
+++ admin/edit-event/edit-class/index.php
@@ -52,7 +52,6 @@
       $classDataQuery = "SELECT * FROM classes WHERE classes.id = ANY('{$classIDList}') AND classes.date_of_class >= '{$todaysDate}';";
       $classData = pg_fetch_row(pg_query($db_connection, $classDataQuery), 0, PGSQL_ASSOC);
 
-      var_dump($classData);
 
       //get start and end dates for entire class range
       $startDate = pg_fetch_array(pg_query($db_connection, "SELECT MIN (date_of_class) AS start_date FROM classes WHERE class_type = '{$selectedClassType}' AND clients <@ '{$clientIDPGArray}' AND (archived IS NULL OR archived = '');"), 0, 1)['start_date'];

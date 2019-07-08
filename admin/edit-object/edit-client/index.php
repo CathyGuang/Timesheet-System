88bffcedf00218340b8f5e116b92d5327396e340
padillac <shinimaninima@gmail.com>
2019-07-08 14:50:52 -0500

    adding permanent delete option to people

--- admin/edit-object/edit-client/index.php
+++ admin/edit-object/edit-client/index.php
@@ -56,7 +56,8 @@ EOT;
         <input type="number" name="phone" maxlength="10" value="{$clientInfo['phone']}">
 
         <p style='color: var(--dark-red)'>Archive: <input type="checkbox" name="archive" value="TRUE"></p>
-        <p style='color: var(--dark-red)'>Delete: <input type="checkbox" name="delete" value="TRUE"> (Cannot be undone)</p>
+        <p style='color: var(--dark-red)'>Delete: <input type="checkbox" name="delete" value="TRUE"></p>
+        <p style='font-size: 12;'>(Cannot be undone)</p>
 
 
         <br><br>

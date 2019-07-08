79459efe6dd9d58d248c4aca0e584889841fb683
padillac <shinimaninima@gmail.com>
2019-07-08 13:00:46 -0500

    bugfixing edit class override

--- admin/edit-event/edit-class/edit-class-override.php
+++ admin/edit-event/edit-class/edit-class-override.php
@@ -21,32 +21,7 @@
   <?php
   //Get post data
   $_POST = unserialize($_POST['override-post']);
-
-
-    if ($_POST['DELETE']) { //DELETE CLASS IF DELETE IS REQUESTED
-      $query = "DELETE FROM classes WHERE class_type = '{$_POST['old-class-type']}' AND clients <@ '{$_POST['old-client-id-list']}' AND (archived IS NULL OR archived = '');";
-      $result = pg_query($db_connection, $query);
-      if ($result) {
-        echo "<h3 class='main-content-header'>Success</h3";
-      } else {
-        echo "<h3 class='main-content-header'>An error occured.</h3><p class='main-content-header'>Please try again, ensure that all data is correctly formatted.</p>";
-      }
-      return;
-    }
-
-    if ($_POST['archive']) { //ARCHIVE CLASS IF REQUESTED
-      $query = "UPDATE classes SET archived = 'TRUE' WHERE class_type = '{$_POST['old-class-type']}' AND clients <@ '{$_POST['old-client-id-list']}';";
-      $result = pg_query($db_connection, $query);
-      if ($result) {
-        echo "<h3 class='main-content-header'>Success</h3";
-      } else {
-        echo "<h3 class='main-content-header'>An error occured.</h3><p class='main-content-header'>Please try again, ensure that all data is correctly formatted.</p>";
-      }
-      return;
-    }
-
-
-
+  var_dump($_POST);
 
     //GET TODAYS' DATE AND ONLY MODIFY CLASSES AFTER TODAYS DATE
     $todaysDate = date('Y-m-d');

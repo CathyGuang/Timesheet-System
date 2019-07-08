513b1053543e86204ab5f1d38b1abd419fc5df75
padillac <shinimaninima@gmail.com>
2019-07-08 12:20:05 -0500

    updating new class override and edit class pages to use new functions4

--- admin/edit-event/edit-class/edit-class.php
+++ admin/edit-event/edit-class/edit-class.php
@@ -113,6 +113,7 @@
     //Create SQL query
     $query = "INSERT INTO classes (class_type, display_title, date_of_class, start_time, end_time, all_weekdays_times, arena, horses, tacks, tack_notes, client_equipment_notes, pads, clients, attendance, staff, volunteers) VALUES";
     foreach ($dateTimeTriplets as $date => $timeArray) {
+      echo "PLUS ONE";
       $query = $query . "('{$_POST['class-type']}', '{$displayTitle}', '{$date}', '{$timeArray[0]}', '{$timeArray[1]}', '$all_weekdays_times', '{$_POST['arena']}', '{$horseIDList}', '{$tackList}', '{$tackNotes}', '{$clientEquipmentNotes}', '{$padList}', '{$clientIDList}', '{$clientIDList}', '{$staffJSON}', '{$volunteerJSON}'),";
     }
 

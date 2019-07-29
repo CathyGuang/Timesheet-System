24da2cd7738f37e167cd0e38b78f4915c58ba066
padillac <shinimaninima@gmail.com>
2019-07-28 20:43:31 -0500

    bugfixing create new horse care shift

--- admin/create-new-event/new-volunteer-shift/new-horse-care-shift/create-new-horse-care-shift.php
+++ admin/create-new-event/new-volunteer-shift/new-horse-care-shift/create-new-horse-care-shift.php
@@ -45,22 +45,6 @@
     }
     //Convert other user selections to database ids
 
-    function to_pg_array($set) {
-      settype($set, 'array'); // can be called with a scalar or array
-      $result = array();
-      foreach ($set as $t) {
-          if (is_array($t)) {
-              $result[] = to_pg_array($t);
-          } else {
-              $t = str_replace('"', '\\"', $t); // escape double quote
-              if (! is_numeric($t)) // quote only non-numeric values
-                  $t = '"' . $t . '"';
-              $result[] = $t;
-          }
-      }
-      return '{' . implode(",", $result) . '}'; // format
-    }
-
     $leaderID = pg_fetch_row(pg_query($db_connection, "SELECT id FROM workers WHERE name LIKE '{$_POST['leader']}' AND (archived IS NULL OR archived = '');"))[0];
     if (!$leaderID) {
       $leaderID = 'null';

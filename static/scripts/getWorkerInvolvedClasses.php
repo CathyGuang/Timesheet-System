7ec43ced7755f5ca0b57592e04e632084c0743aa
padillac <shinimaninima@gmail.com>
2019-08-14 12:21:09 -0700

    adding 'show old classes' option to staff manage classes

--- static/scripts/getWorkerInvolvedClasses.php
+++ static/scripts/getWorkerInvolvedClasses.php
@@ -1,7 +1,7 @@
 <?php
   if ($QUERY_NAME == "ALL") {
     $query = <<<EOT
-    SELECT * FROM classes;
+    SELECT * FROM classes WHERE (archived IS NULL OR archived = '');
 EOT;
   } else {
     $queryID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM workers WHERE workers.name = '{$QUERY_NAME}' AND (archived IS NULL OR archived = '');"), 0, 1)['id'];
--- static/scripts/scheduleGenerator.php
+++ static/scripts/scheduleGenerator.php
@@ -4,8 +4,6 @@
     $selectedName = $_POST['selected-name'];
     $selectedDate = $_POST['selected-date'];
 
-    $FETCH_OLD_CLASSES = false;
-
     $QUERY_NAME = $selectedName;
     //these scripts generate the variables $allClasses, $allOfficeShifts, $allHorseCareShifts
     include $_SERVER['DOCUMENT_ROOT']."/static/scripts/getWorkerInvolvedClasses.php";

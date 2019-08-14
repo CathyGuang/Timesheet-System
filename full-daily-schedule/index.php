0f248bb8952c6f8f96dfe554fdfce88ec5aae8c4
padillac <shinimaninima@gmail.com>
2019-08-14 11:49:23 -0700

    adding date selector to daily schedules

--- full-daily-schedule/index.php
+++ full-daily-schedule/index.php
@@ -24,7 +24,7 @@
       <button class="green-button" type="submit" form="new-schedule" name="selected-date" value="<?php echo date("Y-m-d", strtotime($_POST['selected-date'] . " -1 day")); ?>" style="height: 40pt; max-width: 300px;">PREVIOUS DAY</button>
       <div>
         <input type="date" form="new-schedule" name="selected-date" value="<?php echo date('Y-m-d'); ?>" style="height: 40pt; max-width: 300px;">
-        <button type="submit" form="new-schedule" style="height: 40pt; max-width: 50px;">GO</button>
+        <button class="green-button" type="submit" form="new-schedule" style="height: 40pt; max-width: 50px;">GO</button>
       </div>
       <button class="green-button" type="submit" form="new-schedule" name="selected-date" value="<?php echo date("Y-m-d", strtotime($_POST['selected-date'] . " +1 day")); ?>" style="height: 40pt; max-width: 300px;">NEXT DAY</button>
     </div>

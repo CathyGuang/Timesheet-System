d18a6affd3db87fcff84f3fcc906927a08e9b724
padillac <shinimaninima@gmail.com>
2019-08-26 19:51:59 -0500

    debugging generateClassCode function

--- static/scripts/functions.php
+++ static/scripts/functions.php
@@ -222,7 +222,7 @@ function prepClassDataForSQL($convertedData) {
 
 
 // Function to generate unique class code for creating and editing classes
-function generateClassCode() {
+function generateClassCode($db_connection) {
   var_dump($db_connection);
   $classCode = pg_query($db_connection, "SELECT MAX(class_code) FROM classes;");
   var_dump($classCode);

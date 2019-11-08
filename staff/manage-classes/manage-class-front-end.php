ecf525e2e9e85708112caed89b57fd9cda2627cd
padillac <shinimaninima@gmail.com>
2019-11-08 17:40:34 -0600

    adding input fields to manage class front end

--- staff/manage-classes/manage-class-front-end.php
+++ staff/manage-classes/manage-class-front-end.php
@@ -20,14 +20,21 @@
     $classID = explode(';', $_GET['buttonInfo'])[0];
     $clientString = explode(';', $_GET['buttonInfo'])[1];
 
-    $getClassInfoQuery = "SELECT display_title, class_type, cancelled, date_of_class, lesson_plan, horses, horse_behavior, horse_behavior_notes, clients, attendance, client_notes, staff, volunteers FROM classes WHERE id = {$classID}";
+    $getClassInfoQuery = "SELECT * FROM classes WHERE id = {$classID}";
     $classInfo = pg_fetch_all(pg_query($db_connection, $getClassInfoQuery))[0];
     echo "<h3 class='main-content-header'>{$classInfo['display_title']}, {$classInfo['date_of_class']}</h3>";
   ?>
 
   <form autocomplete="off" action="manage-class-back-end.php" method="post" class="main-form">
 
-    <input type="text" name="id" value="<?php echo $classID ?>" style="visibility: hidden; height: 1px;">
+    <input type="text" name="id" value="<?php echo $classID; ?>" style="visibility: hidden; height: 1px;">
+
+
+
+    <p>Date of Class:</p>
+    <input type="date" id="date-of-class" name="date-of-class" value="<?php echo $classInfo['date_of_class'] ?>" required>
+
+
 
     <p>Lesson Plan:</p>
     <textarea name="lesson-plan" rows="8" cols="30">
@@ -35,6 +42,12 @@
         echo trim($classInfo['lesson_plan']);
       ?>
     </textarea>
+
+
+
+
+
+
 
     <?php $horseNameList = pg_fetch_all_columns(pg_query($db_connection, "SELECT name FROM horses WHERE id = ANY('{$classInfo['horses']}');")); ?>
     <p>Horse(s):</p>
@@ -56,6 +69,10 @@ echo "<option value='$value'>";
         ?>
       </datalist>
 
+
+
+
+
       <p>Horse Behavior:</p>
       <input type="text" name="horse-behavior" list="horse-behavior-enum" value="<?php echo $classInfo['horse_behavior']; ?>">
         <datalist id="horse-behavior-enum">
@@ -70,13 +87,18 @@ echo "<option value='$value'>";
           ?>
         </datalist>
 
-      <br><br>
+
+      <br>
       <p>Horse Behavior Notes:</p>
       <textarea name="horse-behavior-notes" rows="8" cols="30">
         <?php
           echo trim($classInfo['horse_behavior_notes']);
         ?>
       </textarea>
+
+
+
+
 
       <p>Attendance:</p>
       <?php
@@ -107,6 +129,13 @@ EOT;
           }
         ?>
       </datalist>
+
+
+
+
+
+
+
 
 
       <p>Client Notes:</p>
@@ -150,6 +179,12 @@ EOT;
       </datalist>
 
 
+
+
+
+
+
+
       <?php
         $rawArray = explode(",", ltrim(rtrim($classInfo['volunteers'], '}'), '{'));
         $classInfo['volunteers'] = array();
@@ -183,10 +218,20 @@ EOT;
       </datalist>
 
 
-      <br>
+
+
+
+
+
+
 
     <?php if ($classInfo['cancelled'] == "t") {$checked = "checked";} else {$checked = "";} ?>
     <p>Cancel Class: <input type="checkbox" name="cancel" value="TRUE" <?php echo $checked; ?>></p>
+
+
+
+
+
 
 
     <br><br>

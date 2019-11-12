542dda67068ec6f7b1c7e094ddbcedaa43bee742
padillac <shinimaninima@gmail.com>
2019-11-11 22:30:20 -0600

    changing back button on manage class pages to reload pages

--- admin/editable-daily-schedule/index.php
+++ admin/editable-daily-schedule/index.php
@@ -5,14 +5,12 @@
   <link rel="stylesheet" href="/static/main.css">
   <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?>
   <title>Editable Daily Schedule</title>
-  <script>
-    $(window).bind("pageshow", function(event) {
-      if (event.originalEvent.persisted) {
-        alert("it works");
-        window.location.reload();
-      }
-    });
-  </script>
+  <?php
+    //No Cache
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+  ?>
 </head>
 
 <body>

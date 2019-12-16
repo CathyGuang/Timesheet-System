9fce238cd82db850d0176a3f3e28a958d7362524
padillac <shinimaninima@gmail.com>
2019-12-16 14:38:00 -0600

    fixed new client form

--- admin/create-new-object/new-client/index.php
+++ admin/create-new-object/new-client/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="/static/main.css">
-<link href="https://fonts.googleapis.com/css?family=Nunito:700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:700&display=swap" rel="stylesheet">
   <title>Admin | New Client</title>
 </head>
 
@@ -17,36 +17,37 @@
     </nav>
   </header>
 
+  <div class="form-container">
+    <form autocomplete="off" action="create-new-client.php" method="post" class="standard-form">
 
-  <form autocomplete="off" action="create-new-client.php" method="post" class="standard-form">
-
-    <div class="form-section">
-      <div class="form-element">
-        <label for="name">Name/Initials:</label>
-        <input type="text" name="name" id="name" value="" required>
+      <div class="form-section">
+        <div class="form-element">
+          <label for="name">Name/Initials:</label>
+          <input type="text" name="name" id="name" value="" required>
+        </div>
       </div>
-    </div>
 
-    <div class="form-section">
-      <div class="form-element">  
-        <p>Email:</p>
-        <input type="email" name="email" value="">
+      <div class="form-section">
+        <div class="form-element">  
+          <p>Email:</p>
+          <input type="email" name="email" value="">
+        </div>
       </div>
-    </div>
 
-    <div class="form-section">
-      <div class="form-element">  
-        <p>Phone Number:</p>
-        <input type="number" name="phone" maxlength="10" value="">
+      <div class="form-section">
+        <div class="form-element">  
+          <p>Phone Number:</p>
+          <input type="number" name="phone" maxlength="10" value="">
+        </div>
       </div>
-    </div>
 
-    <div class="form-section">
-      <button type="button" class="cancel-form" onclick="window.history.back()">Cancel</button>
-      <button type="submit">Create</button>
-    </div>
+      <div class="form-section">
+        <button type="button" class="cancel-form" onclick="window.history.back()">Cancel</button>
+        <button type="submit">Create</button>
+      </div>
 
-  </form>
+    </form>
+  </div>
 
 
 

5cfdba452f848ce1767a3085d1859f79f31c1b27
padillac <shinimaninima@gmail.com>
2019-12-17 19:00:39 -0600

    updated create new other form

--- admin/create-new-object/new-other/index.php
+++ admin/create-new-object/new-other/index.php
@@ -19,43 +19,53 @@
     </nav>
   </header>
 
+  <div class="form-container">
+    <form autocomplete="off" action="create-new-other.php" method="post" class="standard-form">
 
-  <form autocomplete="off" action="create-new-other.php" method="post" class="standard-form">
-
-    <p>Select object type:</p>
-    <input name="object-type" list="object-type-list" onclick="select();" required>
+      <div class="form-section">
+        <div class="form-element">
+          <label for="object-type">Select object type:</label>
+          <input name="object-type" id="object-type" list="object-type-list" onclick="select();" required>
+        </div>
+      </div>
+      
       <datalist id="object-type-list">
         <?php
           $query = <<<EOT
           SELECT n.nspname as "Schema",
-  pg_catalog.format_type(t.oid, NULL) AS "Name",
-  pg_catalog.obj_description(t.oid, 'pg_type') as "Description"
-FROM pg_catalog.pg_type t
-     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
-WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
-  AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
-      AND n.nspname <> 'pg_catalog'
-      AND n.nspname <> 'information_schema'
-  AND pg_catalog.pg_type_is_visible(t.oid)
-ORDER BY 1, 2;
+            pg_catalog.format_type(t.oid, NULL) AS "Name",
+            pg_catalog.obj_description(t.oid, 'pg_type') as "Description"
+          FROM pg_catalog.pg_type t
+          LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+          WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
+          AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
+          AND n.nspname <> 'pg_catalog'
+          AND n.nspname <> 'information_schema'
+          AND pg_catalog.pg_type_is_visible(t.oid)
+          ORDER BY 1, 2;
 EOT;
           $result = pg_query($db_connection, $query);
-
+          
           while ($row = pg_fetch_row($result)) {
             echo "<option value='{$row[1]}'>";
           }
         ?>
       </datalist>
 
-    <p>Name of new object:</p>
-    <input type="text" name="new-object-name" value="" required>
 
+      <div class="form-section">
+        <div class="form-element">
+          <p>Name of new object:</p>
+          <input type="text" name="new-object-name" value="" required>
+        </div>
+      </div>
 
-    <br><br>
-    <button type="submit">Submit</button>
+      <div class="form-section">
+        <button type="submit">Submit</button>
+      </div>
 
-  </form>
-
+    </form>
+  </div>
 
 
 

d11dbc2b78507176f4d8986a184d35832d0db109
padillac <shinimaninima@gmail.com>
2019-12-21 23:23:27 -0500

    reverting

--- admin/edit-event/edit-class/class-editor.php
+++ admin/edit-event/edit-class/class-editor.php
@@ -434,7 +434,6 @@ EOF;
           <div class="form-section">
             <h3>Volunteers:</h3>
           </div>
-          <div class='form-section'>
           <?php
             $volunteerData = json_decode($classData['volunteers']);
             if ($volunteerData) {
@@ -442,21 +441,20 @@ EOF;
                 $volunteerName = pg_fetch_array(pg_query($db_connection, "SELECT name FROM workers WHERE workers.id = {$volunteerID};"), 0, 1)['name'];
                 $volunteer = htmlspecialchars($volunteerName, ENT_QUOTES);
                 $role = htmlspecialchars($role, ENT_QUOTES);
-                echo "<div class='form-element'>";
+                echo "<div class='form-section'><div class='form-element'>";
                 echo "<label>Role:</label><input form='class-form' type='text' name='volunteer-roles[]' list='volunteer-role-list' value='{$role}' onclick='select();'>";
                 echo "</div><div class='form-element'>";
                 echo "<label>Volunteer:</label><input form='class-form' type='text' name='volunteers[]' list='volunteer-list' value='{$volunteer}' onclick='select();'>";
-                echo "</div>";
+                echo "</div></div>";
               }
             } else {
-              echo "<div class='form-element'>";
+              echo "<div class='form-section'><div class='form-element'>";
               echo "<label>Role:</label><input form='class-form' type='text' name='volunteer-roles[]' list='volunteer-role-list' value='' onclick='select();'>";
               echo "</div><div class='form-element'>";
               echo "<label>Volunteer:</label><input form='class-form' type='text' name='volunteers[]' list='volunteer-list' value='' onclick='select();'>";
-              echo "</div>";
+              echo "</div></div>";
             }
           ?>
-          </div>
         </div>
         <div class="form-section">
           <div class="form-element">

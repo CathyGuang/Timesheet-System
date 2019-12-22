b97b00687923637c8de18e538469ce065d0c9d5a
padillac <shinimaninima@gmail.com>
2019-12-22 12:28:35 -0500

    textarea

--- admin/create-new-object/new-horse/index.php
+++ admin/create-new-object/new-horse/index.php
@@ -96,7 +96,7 @@
 
       <div class='form-section'>
         <div class='form-element'>
-          <textarea rows=3 name="notes" id="notes" value="">
+          <textarea rows=3 name="notes" id="notes"></textarea>
         </div>
       </div>
 

dc40728fb37055b6cc86c11577173fa8e2713502
padillac <shinimaninima@gmail.com>
2019-12-22 12:30:14 -0500

    textarea

--- admin/edit-object/edit-horse/index.php
+++ admin/edit-object/edit-horse/index.php
@@ -21,22 +21,34 @@
   <?php
     if (!$_POST['selected-horse']) {
       echo <<<EOT
-      <form autocomplete="off" action="" method="post" class="standard-form">
-        <p>Select a horse to edit:</p>
-        <input type="text" name="selected-horse" list="horse-list" onclick='select();'>
-          <datalist id="horse-list">
-EOT;
-              $query = "SELECT name FROM horses WHERE name != 'HORSE NEEDED' AND (archived IS NULL OR archived = '');";
-              $result = pg_query($db_connection, $query);
-              while ($row = pg_fetch_row($result)) {
-                echo "<option value='$row[0]'>";
-              }
-      echo <<<EOT
-          </datalist>
+      <div class="form-container">
+        <form autocomplete="off" action="" method="post" class="standard-form">
+          <div class="form-section">
+            <h3>Select a horse to edit:</h3>
+          </div>
 
-          <br><br>
-          <button type="submit">Submit</button>
-      </form>
+          <div class="form-section">
+            <div class="form-element">
+              <input type="text" name="selected-horse" list="horse-list" onclick='select();'>
+            </div>
+          </div>
+
+            <datalist id="horse-list">
+EOT;
+                $query = "SELECT name FROM horses WHERE name != 'HORSE NEEDED' AND (archived IS NULL OR archived = '');";
+                $result = pg_query($db_connection, $query);
+                while ($row = pg_fetch_row($result)) {
+                  echo "<option value='$row[0]'>";
+                }
+        echo <<<EOT
+            </datalist>
+
+            <div class="form-section">
+              <button type="button" class="cancel-form" onclick="window.history.back()">Cancel</button>
+              <button type="submit">Go</button>
+            </div>
+        </form>
+      </div>
 EOT;
     } else {
       $horseInfoQuery = "SELECT * FROM horses WHERE name LIKE '{$_POST['selected-horse']}' AND (archived IS NULL OR archived = '');";
@@ -44,55 +56,100 @@ EOT;
       $horseInfo = pg_fetch_array($horseInfoSQL, 0, PGSQL_ASSOC);
 
       echo <<<EOT
-      <form autocomplete="off" action="edit-horse.php" method="post" class="standard-form" style="margin-top: 2vh;">
+      <div class="form-container">
+        <form autocomplete="off" action="edit-horse.php" method="post" class="standard-form" style="margin-top: 2vh;">
 
+          <div class=form=section>
+            <div class="form-element">
+              <label for="name">Name:</label>
+              <input type="text" name="name" id="name" value="{$horseInfo['name']}" required>
+            </div>
+          </div>
 
-        <p>Name:</p>
-        <input type="text" name="name" value="{$horseInfo['name']}" required>
+          <div class='form-section'>
+            <h3>Owner:</h3>
+          </div>
 
-        <p>Owner:</p>
-        <label>(Leave blank if owner is this organization)</label>
-        <input type="text" name="owner" value="{$horseInfo['owner']}">
+          <div class=form=section>
+            <div class="form-element">
+              <label for="owner">Owner: (Leave blank for organization)</label>
+              <input type="text" name="owner" id="owner" value="{$horseInfo['owner']}">
+            </div>
 
-        <p>Owner email address:</p>
-        <input type="email" name="owner-email" value="{$horseInfo['owner_email']}">
+            <div class="form-element">
+              <label for="owner-email">Owner email address:</label>
+              <input type="email" name="owner-email" id="owner-email" value="{$horseInfo['owner_email']}">
+            </div>
 
-        <p>Owner phone number:</p>
-        <input type="phone" name="owner-phone" value="{$horseInfo['owner_phone']}">
+            <div class="form-element">
+              <label for="owner-phone">Owner phone number:</label>
+              <input type="phone" name="owner-phone" id="owner-phone" value="{$horseInfo['owner_phone']}">
+            </div>
+          </div>
 
-        <p>Veterinarian name:</p>
-        <input type="text" name="vet-name" value="{$horseInfo['vet_name']}">
+          <div class='form-section'>
+            <h3>Veterinarian:</h3>
+          </div>
 
-        <p>Veterinarian email address:</p>
-        <input type="email" name="vet-email" value="{$horseInfo['vet_email']}">
+          <div class=form=section>
+            <div class="form-element">
+              <label for="vet-name">Veterinarian name:</label>
+              <input type="text" name="vet-name" id="vet-name" value="{$horseInfo['vet_name']}">
+            </div>
 
-        <p>Veterinarian phone number:</p>
-        <input type="phone" name="vet-phone" value="{$horseInfo['vet_phone']}">
+            <div class="form-element">
+              <label for="vet-email">Veterinarian email address:</label>
+              <input type="email" name="vet-email" id="vet-email" value="{$horseInfo['vet_email']}">
+            </div>
 
-        <p>Organization Uses per Day:</p>
-        <input type="number" name="org_uses_per_day" value="{$horseInfo['org_uses_per_day']}">
+            <div class="form-element">
+              <label for="vet-phone">Veterinarian phone number:</label>
+              <input type="phone" name="vet-phone" id="vet-phone" value="{$horseInfo['vet_phone']}">
+            </div>
+          </div>
 
-        <p>Owner Uses per Day:</p>
-        <input type="number" name="owner_uses_per_day" value="{$horseInfo['owner_uses_per_day']}">
+          <div class='form-section'>
+            <h3>Use:</h3>
+          </div>
 
-        <p>Horse Uses per Week:</p>
-        <input type="number" name="horse_uses_per_week" value="{$horseInfo['horse_uses_per_week']}" required>
+          <div class=form=section>
+            <div class="form-element">
+              <label for="org_uses_per_day">Organization Uses per Day:</label>
+              <input type="number" name="org_uses_per_day" id="org_uses_per_day" value="{$horseInfo['org_uses_per_day']}">
+            </div>
 
-        <div>
-          <p>Notes:</p>
-          <input type="text" name="notes" value="{$horseInfo['notes']}">
-        </div>
+            <div class="form-element">
+              <label for="owner_uses_per_day">Owner Uses per Day:</label>
+              <input type="number" name="owner_uses_per_day" id="owner_uses_per_day" value="{$horseInfo['owner_uses_per_day']}">
+            </div>
 
-        <p style='color: var(--dark-red)'>Archive: <input type="checkbox" name="archive" value="TRUE"></p>
-        <p style='color: var(--dark-red);'>Delete: <input type="checkbox" name="delete" value="TRUE"></p>
-        <p style='font-size: 10pt; color: var(--dark-red); margin-top: 0;'>(Cannot be undone)</p>
+            <div class="form-element">
+              <label for="horse_uses_per_week">Horse Uses per Week:</label>
+              <input type="number" name="horse_uses_per_week" id="horse_uses_per_week" value="{$horseInfo['horse_uses_per_week']}" required>
+            </div>
+          </div>
 
-        <br><br>
-        <button type="submit">Update</button>
+          <div class='form-section'>
+            <h3>Notes:</h3>
+          </div>
 
-        <input type="number" name="id" value="{$horseInfo['id']}" hidden>
+          <div class=form=section>
+            <div class="form-element">
+              <div>
+                <textarea name="notes">{$horseInfo['notes']}</textarea>
+              </div>
 
-      </form>
+          <p style='color: var(--dark-red)'>Archive: <input type="checkbox" name="archive" value="TRUE"></p>
+          <p style='color: var(--dark-red);'>Delete: <input type="checkbox" name="delete" value="TRUE"></p>
+          <p style='font-size: 10pt; color: var(--dark-red); margin-top: 0;'>(Cannot be undone)</p>
+
+          <br><br>
+          <button type="submit">Update</button>
+
+          <input type="number" name="id" value="{$horseInfo['id']}" hidden>
+
+        </form>
+      </div>
 EOT;
     }
   ?>

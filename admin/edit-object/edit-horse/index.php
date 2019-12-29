866cbd988d2f9f99044110d745add8808cb48650
Chris Padilla <shinimaninima@gmail.com>
2019-12-29 15:43:32 -0500

    updated edit horse form

--- admin/edit-object/edit-horse/index.php
+++ admin/edit-object/edit-horse/index.php
@@ -162,7 +162,7 @@ EOT;
 
           <div class="form-section">
             <button type="button" class="cancel-form" onclick="window.history.back(2)">Cancel</button>
-            <button type="submit">Submit</button>
+            <button type="submit">Update</button>
           </div>
 
           <input type="number" name="id" value="{$horseInfo['id']}" hidden>

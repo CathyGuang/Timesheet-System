77797c93f86e0d5157a9d0a2487138cccb2ec5bc
Chris Padilla <shinimaninima@gmail.com>
2019-12-30 16:23:03 -0500

    fixed volunteer record hours form

--- volunteer/record-hours/index.php
+++ volunteer/record-hours/index.php
@@ -18,56 +18,89 @@
   </header>
 
 
-
+  <div class="form-container">
     <form autocomplete="off" class="standard-form" action="volunteer-record-hours.php" method="post">
 
 
-      <p>Name:</p>
-      <input type="text" name="volunteer" list="volunteer-list" value="<?php echo $_POST['name']; ?>" required>
-        <datalist id="volunteer-list">
-          <?php
-            $volunteerNames = pg_fetch_all_columns(pg_query($db_connection, "SELECT name FROM workers WHERE volunteer = TRUE AND (archived IS NULL OR archived = '');"));
-            foreach ($volunteerNames as $name) {
-              $name = htmlspecialchars($name, ENT_QUOTES);
-              echo "<option value='{$name}'>";
-            }
-          ?>
-        </datalist>
+      <div class="form-section">
+        <div class="form-element">
+          <label>Name:</label>
+          <input type="text" name="volunteer" list="volunteer-list" value="<?php echo $_POST['name']; ?>" required>
+        </div>
+      </div>
 
-      <p>Type of shift:</p>
-      <input type="text" name="shift-type" list="shift-type-list" required>
-        <datalist id="shift-type-list">
-          <?php
-            $horseCareShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::CARE_TYPE));"));
-            foreach ($horseCareShiftTypes as $value) {
-              echo "<option value='{$value}'>";
-            }
-            $officeShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::OFFICE_SHIFT_TYPE));"));
-            foreach ($officeShiftTypes as $value) {
-              echo "<option value='{$value}'>";
-            }
-            $classShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::VOLUNTEER_CLASS_ROLE));"));
-            foreach ($classShiftTypes as $value) {
-              echo "<option value='{$value}'>";
-            }
-          ?>
-        </datalist>
+      <div class="form-section">
+        <div class="form-element">
+          <label for="shift-type">Type of shift:</label>
+          <input type="text" name="shift-type" id="shift-type" list="shift-type-list" required>
+        </div>
+      </div>
 
-      <p>Date:</p>
-      <input type="date" name="date-of-hours" value="<?php echo date('Y-m-d'); ?>" required>
+     <div class="form-section">
+        <div class="form-element">
+          <label>Date:</label>
+          <input type="date" name="date-of-hours" value="<?php echo date('Y-m-d'); ?>" required>
+        </div>
+      </div>
 
-      <p>Number of hours:</p>
-      <input type="text" name="hours" required>
+      <div class="form-section">
+        <div class="form-element">
+          <label>Number of hours:</label>
+          <input type="text" name="hours" required>
+        </div>
+      </div>
 
-      <p>Notes:</p>
-      <input type="text" name="notes">
+      <div class="form-section">
+        <div class="form-element">
+          <label>Notes:</label>
+          <input type="text" name="notes">
+        </div>
+      </div>
 
-      <p>Notify Supervisor: <input type="checkbox" name="send-email" value="true"></p>
+      <div class="form-section">
+        <div class="form-element">
+          <p>Notify supervisor: <input type="checkbox" name="send-email" value="true"></p>
+        </div>
+      </div>
 
 
-      <br><br>
-      <button type="submit">Submit</button>
+      <div class="form-section">
+        <button type="button" class="cancel-form" onclick="window.history.back()">Cancel</button>
+        <button type="submit">Submit</button>
+      </div>
+
     </form>
+  </div>
+
+
+  <!-- DATALISTS -->
+  <datalist id="volunteer-list">
+    <?php
+      $volunteerNames = pg_fetch_all_columns(pg_query($db_connection, "SELECT name FROM workers WHERE volunteer = TRUE AND (archived IS NULL OR archived = '');"));
+      foreach ($volunteerNames as $name) {
+        $name = htmlspecialchars($name, ENT_QUOTES);
+        echo "<option value='{$name}'>";
+      }
+    ?>
+  </datalist>
+
+  <datalist id="shift-type-list">
+    <?php
+      $horseCareShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::CARE_TYPE));"));
+      foreach ($horseCareShiftTypes as $value) {
+        echo "<option value='{$value}'>";
+      }
+      $officeShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::OFFICE_SHIFT_TYPE));"));
+      foreach ($officeShiftTypes as $value) {
+        echo "<option value='{$value}'>";
+      }
+      $classShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::VOLUNTEER_CLASS_ROLE));"));
+      foreach ($classShiftTypes as $value) {
+        echo "<option value='{$value}'>";
+      }
+    ?>
+  </datalist>
+
 
 
 </body>

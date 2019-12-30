5f961941dad8840973250356ce0f884c9f1c5907
Chris Padilla <shinimaninima@gmail.com>
2019-12-30 11:54:09 -0500

    updated schedule header

--- horse-daily-schedule/schedule.php
+++ horse-daily-schedule/schedule.php
@@ -21,9 +21,9 @@
 
     <div style="display: flex; justify-content: space-between;">
       <button class="green-button" type="submit" form="new-schedule2" name="selected-date" value="<?php echo date("Y-m-d", strtotime($_POST['selected-date'] . " -1 day")); ?>" style="height: 40pt; max-width: 300px;">PREVIOUS DAY</button>
-      <div>
+      <div style="display: inherit;">
         <input type="date" form="new-schedule" name="selected-date" value="<?php echo $_POST['selected-date']; ?>" style="height: 40pt; max-width: 300px;">
-        <button class="green-button" type="submit" form="new-schedule" style="height: 40pt; max-width: 50px;">GO</button>
+        <button class="green-button" type="submit" form="new-schedule" style="height: 41.5pt; max-width: 50px;">GO</button>
       </div>
       <button class="green-button" type="submit" form="new-schedule" name="selected-date" value="<?php echo date("Y-m-d", strtotime($_POST['selected-date'] . " +1 day")); ?>" style="height: 40pt; max-width: 300px;">NEXT DAY</button>
     </div>

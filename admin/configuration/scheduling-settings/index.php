e245f521b4f987ba9a7205fe42bff3850f9b6fbd
padillac <shinimaninima@gmail.com>
2020-01-11 10:47:08 -0600

    test

--- admin/configuration/scheduling-settings/index.php
+++ admin/configuration/scheduling-settings/index.php
@@ -20,8 +20,8 @@
 
 
   <?php
-      $ignoreTack = pg_fetch_all(pg_query($db_connection, "SELECT value FROM misc_data WHERE key LIKE 'ignore_tack_conflicts';"));
-      $ignorePad = pg_fetch_all(pg_query($db_connection, "SELECT value FROM misc_data WHERE key LIKE 'ignore_pad_conflicts';"));
+      $ignoreTack = pg_fetch_row(pg_query($db_connection, "SELECT value FROM misc_data WHERE key LIKE 'ignore_tack_conflicts';"), 0)['value'];
+      $ignorePad = pg_fetch_row(pg_query($db_connection, "SELECT value FROM misc_data WHERE key LIKE 'ignore_pad_conflicts';"), 0)['value'];
       var_dump($ignorePad);
       var_dump($ignoreTack);
     ?>

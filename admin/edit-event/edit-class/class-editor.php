dd4fc449b958ca5f9d4ef9dc6098ac9cb3f3f374
padillac <shinimaninima@gmail.com>
2020-01-14 11:37:55 -0600

    fixing cancel button

--- admin/edit-event/edit-class/class-editor.php
+++ admin/edit-event/edit-class/class-editor.php
@@ -506,7 +506,7 @@
 
 
       <div class="form-section">
-        <button type="button" class="cancel-form" onclick="window.location.href = ../">Cancel</button>
+        <a href="../"><button type="button" class="cancel-form">Cancel</button></a>
         <button type="submit" <?php if ($pastClass) {echo "disabled";} ?>>Submit</button>
       </div>      
 

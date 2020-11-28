19cee98fbed4184d5fd969799fa2e295a3019b35
padillac <shinimaninima@gmail.com>
2020-11-27 18:52:50 -0800

    Added org-specific-info-TEMPLATE directory to git repo, as well as notes and scripts txt files.

---
+++ Org-Specific-Info-TEMPLATE/info.php
@@ -0,0 +1,20 @@
+<?php
+//CHANGE EVERYTHING IN BRACKETS
+
+//Organization Name
+$organizationName = "[ORGANIZATION NAME]";
+
+//Time Zone ('America/Los_Angeles', 'America/Denver', 'America/Chicago', 'America/New_York')
+$organizationTimeZone = "America/[ORGANIZATION REGION]";
+
+
+//Database Login Credentials
+$database = "[DATABASE NAME]";
+$dbuser = "[DATABASE ADMIN NAME]";
+$dbpassword = "[DATABASE PASSWORD]";
+
+
+
+
+
+?>

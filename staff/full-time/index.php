0d74f623718a1980663d7455f5f785e77b6624b2
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-12-07 18:50:27 -0800

    added jquery link

--- staff/full-time/index.php
+++ staff/full-time/index.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="/static/main.css">
   <link rel="stylesheet" href="/static/added.css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:700&display=swap" rel="stylesheet">
-  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script type="text/javascript" src="/static/TimeSlider.js"></script>
   <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?> 
   <title>Full time</title>

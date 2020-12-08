42cadbd53d4e28c5196ae45fd5a1663905d354c0
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-12-08 08:38:37 -0800

    added time-range slider

--- staff/full-time/index.php
+++ staff/full-time/index.php
@@ -2,10 +2,14 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:700&display=swap" rel="stylesheet">
-  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-  <script type="text/javascript" src="/static/TimeSlider.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-2.0.2.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script type="text/javascript" src="https://rangeslider.js.org/assets/rangeslider.js/dist/rangeslider.min.js"></script>
+  <script type="text/javascript" src="rangeslider.js"></script>
+  <link rel="stylesheet" href="/static/slider.css">
+  <link rel="stylesheet" href="/static/jquery-ui.css">
+  <link rel="stylesheet" href="/static/rangeslider.css">
   <link rel="stylesheet" href="/static/main.css">
   <link rel="stylesheet" href="/static/added.css">
   <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?> 
@@ -21,33 +25,129 @@
       <a href="/"><button id="home-button">Home</button></a>
     </nav>
   </header>
+
+  <div class="time-range" style="right:25em;">
+     <p class="time-range-time"> <span class="slider-time">10:00 AM</span> - <span class="slider-time2">12:00 PM</span>
+
+     </p>
+     <div class="sliders_step1">
+         <div id="slider-range"></div>
+     </div>
+  </div>
+
+  <div class="time-range" style="right:48em;">
+     <p class="time-range-time"> <span class="slider-time1">10:00 AM</span> - <span class="slider-time3">12:00 PM</span>
+
+     </p>
+     <div class="sliders_step1">
+         <div id="slider-range1"></div>
+     </div>
+  </div>
+
+ 
   
-  <div class="top_grid">
-    <form class="form-inline" action="full-time.php" autocomplete="off">
-      <label for="Name" >Name:</label>
-      <input type="Name" id="Name" placeholder="Enter name" name="name">
-      <label>Date:</label>
-      <input type="date" name="date-of-hours" value="<?php echo date('Y-m-d'); ?>" required>
 
-    </form>
-  </div>
+  <div class="worktype">
+    <section class="indirectadmin">
+        <input
+        name="range1"
+        type="range"
+        min="0"                   
+        max="720"                  
+        step="10"                   
+        value="0"                 
+        data-orientation="horizontal" 
+        >
 
-  <div id="time-range">
-    <p>Time Range:
-      <span class="slider-time">9:00 AM</span> -
-      <span class="slider-time2">5:00 PM</span>
-    </p>
-    <div class="sliders_step1">
-        <div id="slider-range" class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all">
-          <div class="ui-slider-range ui-widget-header ui-corner-all"></div>
-          <a class="ui-slider-handle ui-state-default ui-corner-all" ></a>
-          <a class="ui-slider-handle ui-state-default ui-corner-all" ></a>
-        </div>
-    </div>
+        <div class="output_hours"></div>
+
+    </section> 
+
+    <section class="riding">
+        <input
+        name="range2"
+        type="range"
+        min="0"                   
+        max="720"                  
+        step="10"                   
+        value="0"                 
+        data-orientation="horizontal" 
+        >
+
+        <div class="output_hours"></div>
+
+    </section> 
+
+    <section class="vaulting admin">
+        <input
+        name="range3"
+        type="range"
+        min="0"                   
+        max="720"                  
+        step="10"                   
+        value="0"                 
+        data-orientation="horizontal" 
+        >
+
+        <div class="output_hours"></div>
+
+    </section> 
+
+    <section class="vaulting">
+        <input
+        name="range4"
+        type="range"
+        min="0"                   
+        max="720"                  
+        step="10"                   
+        value="0"                 
+        data-orientation="horizontal" 
+        >
+
+        <div class="output_hours"></div>
+
+    </section> 
+
+    <section class="PTO">
+        <input
+        name="range5"
+        type="range"
+        min="0"                   
+        max="720"                  
+        step="10"                   
+        value="0"                 
+        data-orientation="horizontal" 
+        >
+
+        <div class="output_hours"></div>
+
+    </section> 
+
+    <section class="Holiday">
+        <input
+        name="range6"
+        type="range"
+        min="0"                   
+        max="720"                  
+        step="10"                   
+        value="0"                 
+        data-orientation="horizontal" 
+        >
+
+        <div class="output_hours"></div>
+
+    </section> 
+
+
+
   </div>
+  
+  
 
 
 
 </body>
+
+<script type="text/javascript" src="/static/query.js"></script>
 
 </html>

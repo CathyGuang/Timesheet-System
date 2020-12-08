f9c1716801d04643e0347fd6ccb08651b5c92564
CathyGuang <71342754+CathyGuang@users.noreply.github.com>
2020-12-08 14:17:52 -0600

    h

--- staff/full-time/index.php
+++ staff/full-time/index.php
@@ -14,147 +14,153 @@
   <link rel="stylesheet" href="/static/added.css">
   <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?> 
   <title>Full time</title>
- 
-</head>
 
-<body style="background-color:white">
+  </head>
 
-  <header>
-    <h1>Full-time</h1>
-    <nav> <a href="../"><button id="back-button">Back</button></a>
-      <a href="/"><button id="home-button">Home</button></a>
+<body style="background-color:#D0BDF4">
+
+
+  <header class="full-time-header">
+    <p class="full-time-title">Full-time Staff Timesheet</p>
+    <nav class="button-container"> 
+      <a href="../"><button class="back-button">Back</button></a>
+      <a href="/"><button class="home-button">Home</button></a>
     </nav>
   </header>
 
-  <form class="form-inline" action="/full-time.php">
-    <label for="Name">Name</label>
-    <input type="Name" id="Name" placeholder="Enter name" name="Name">
-    <label for="Date">Date:</label>
-    <input type="Date" name="Date" value="<?php echo date('Y-m-d'); ?>" required>
-  
-  </form>
-
-  <div class="time-range-container">
-    <div class="time-range">
-      <p class="time-range-time"> <span class="slider-time">10:00 AM</span> - <span class="slider-time2">12:00 PM</span>
-
-      </p>
-      <div class="sliders_step1">
-          <div id="slider-range"></div>
-      </div>
-    </div>
-
-    <div class="time-range">
-      <p class="time-range-time"> <span class="slider-time1">10:00 AM</span> - <span class="slider-time3">12:00 PM</span>
-
-      </p>
-      <div class="sliders_step1">
-          <div id="slider-range1"></div>
-      </div>
-    </div>
-
-  </div>
-
-
-  
- 
-  
-
-  <div class="worktype">
-    <section class="indirectadmin">
+  <div class= "fulltime-form-container">
+          
+    <form class="form-inline" action="/full-time.php">
+      <label for="Name">Name:</label>
+      <input type="Name" id="Name" placeholder="Enter name" name="Name">
+      <label for="Date">Date:</label>
+      <input type="Date" name="Date" value="<?php echo date('Y-m-d'); ?>" required>
     
-        <input
-        name="range1"
-        type="range"
-        min="0"                   
-        max="720"                  
-        step="10"                   
-        value="0"                 
-        data-orientation="horizontal" 
-        >
+    </form>
 
-        <div class="output_hours"></div>
+    <div class="time-range-container">
+      <div class="time-range">
+        <p class="time-range-time"> <span class="slider-time">10:00 AM</span> - <span class="slider-time2">12:00 PM</span>
 
-    </section> 
+        </p>
+        <div class="sliders_step1">
+            <div id="slider-range"></div>
+        </div>
+      </div>
 
-    <section class="riding">
-        <input
-        name="range2"
-        type="range"
-        min="0"                   
-        max="720"                  
-        step="10"                   
-        value="0"                 
-        data-orientation="horizontal" 
-        >
+      <div class="time-range" style="left:26em;">
+        <p class="time-range-time"> <span class="slider-time1">10:00 AM</span> - <span class="slider-time3">12:00 PM</span>
 
-        <div class="output_hours"></div>
+        </p>
+        <div class="sliders_step1">
+            <div id="slider-range1"></div>
+        </div>
+      </div>
 
-    </section> 
+    </div>
+    <div class= "worktype_row_container">
+      <div class="worktype" style="left:22em">
+        <section class="indirectadmin">
+            <input
+            name="range1"
+            type="range"
+            min="0"                   
+            max="720"                  
+            step="10"                   
+            value="0"                 
+            data-orientation="horizontal" 
+            >
 
-    <section class="vaulting admin">
-        <input
-        name="range3"
-        type="range"
-        min="0"                   
-        max="720"                  
-        step="10"                   
-        value="0"                 
-        data-orientation="horizontal" 
-        >
+            <div class="output_hours"></div>
 
-        <div class="output_hours"></div>
+        </section> 
 
-    </section> 
+        <section class="riding">
+            <input
+            name="range2"
+            type="range"
+            min="0"                   
+            max="720"                  
+            step="10"                   
+            value="0"                 
+            data-orientation="horizontal" 
+            >
 
-    <section class="vaulting">
-        <input
-        name="range4"
-        type="range"
-        min="0"                   
-        max="720"                  
-        step="10"                   
-        value="0"                 
-        data-orientation="horizontal" 
-        >
+            <div class="output_hours"></div>
 
-        <div class="output_hours"></div>
+        </section> 
 
-    </section> 
+        <section class="vaulting admin" >
+            <input
+            name="range3"
+            type="range"
+            min="0"                   
+            max="720"                  
+            step="10"                   
+            value="0"                 
+            data-orientation="horizontal" 
+            >
 
-    <section class="PTO">
-        <input
-        name="range5"
-        type="range"
-        min="0"                   
-        max="720"                  
-        step="10"                   
-        value="0"                 
-        data-orientation="horizontal" 
-        >
+            <div class="output_hours"></div>
 
-        <div class="output_hours"></div>
+        </section> 
+      </div>
+      <div class="worktype" style="left:42em">
+        <section class="vaulting">
+        <input type="text" name="work-type" list="work-type-list" required>
+            <input
+            name="range4"
+            type="range"
+            min="0"                   
+            max="720"                  
+            step="10"                   
+            value="0"                 
+            data-orientation="horizontal" 
+            >
 
-    </section> 
+            <div class="output_hours"></div>
 
-    <section class="Holiday">
-        <input
-        name="range6"
-        type="range"
-        min="0"                   
-        max="720"                  
-        step="10"                   
-        value="0"                 
-        data-orientation="horizontal" 
-        >
+        </section> 
 
-        <div class="output_hours"></div>
+        <section class="PTO">
+            <input
+            name="range5"
+            type="range"
+            min="0"                   
+            max="720"                  
+            step="10"                   
+            value="0"                 
+            data-orientation="horizontal" 
+            >
 
-    </section> 
+            <div class="output_hours"></div>
+
+        </section> 
+
+        <section class="Holiday">
+            <input
+            name="range6"
+            type="range"
+            min="0"                   
+            max="720"                  
+            step="10"                   
+            value="0"                 
+            data-orientation="horizontal" 
+            >
+
+            <div class="output_hours"></div>
+
+        </section> 
 
 
 
+      </div>
+
+    </div>
+    
+  
   </div>
+
   
   
 

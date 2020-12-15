908de46b4810c3a6f6792c1dadb7ffb0160522de
CathyGuang <71342754+CathyGuang@users.noreply.github.com>
2020-12-15 15:33:01 -0600

    aa

--- admin/reports/staff-hours/staff-hours-table.php
+++ admin/reports/staff-hours/staff-hours-table.php
@@ -22,35 +22,37 @@
   $staffName = pg_escape_string(trim($_POST['staff']));
   $staffID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM workers WHERE name = '{$staffName}' AND (archived IS NULL OR archived = '');"), 0, 1)['id'];
 
-  echo $staffName.$staffID;
+  //initialize target table name
+  $tableName = "staff_hours";
+  //Connect to database
+  include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php";
 
+  //delete tempfiles from previous reports
+  if (file_exists("/tmp/DHStempfile.csv")) {
+    unlink("/tmp/DHStempfile.csv");
+  }
 
-  // //initialize target table name
-  // $tableName = "staff_hours";
-  // //Connect to database
-  // include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php";
+  //Get table columns for CSV file
+  $metadata = array();
+  $metadata[0] = pg_fetch_all_columns(pg_query($db_connection, "SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '{$tableName}';"));
 
-  // //delete tempfiles from previous reports
-  // if (file_exists("/tmp/DHStempfile.csv")) {
-  //   unlink("/tmp/DHStempfile.csv");
-  // }
+  //Get table data
+  $result = pg_copy_to($db_connection, "{$tableName}", "%", "");
+  foreach ($result as $key => $dataString) {
+    $result[$key] = explode('%', trim($dataString));
+  }
 
-  // //Get table columns for CSV file
-  // $metadata = array();
-  // $metadata[0] = pg_fetch_all_columns(pg_query($db_connection, "SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '{$tableName}';"));
+  $data = array_merge($metadata, $result);
 
-  // //Get table data
-  // $result = pg_copy_to($db_connection, "{$tableName}", "%", "");
-  // foreach ($result as $key => $dataString) {
-  //   $result[$key] = explode('%', trim($dataString));
-  // }
-
-  // $data = array_merge($metadata, $result);
-
-  // //Write data to temporary CSV file on the server
+  
+  foreach ($data as $line) {
+    echo $line;
+  }
+  //Write data to temporary CSV file on the server
   // $tempfile = fopen('/tmp/DHStempfile.csv', 'w');
 
   // foreach ($data as $line) {
+  //   echo $line;
   //   fputcsv($tempfile, $line);
   // }
 

7a81f58c567572535d84eae0ae1457b2944445be
CathyGuang <71342754+CathyGuang@users.noreply.github.com>
2020-12-15 17:25:47 -0600

    aa

--- admin/reports/staff-hours/staff-hours-table.php
+++ admin/reports/staff-hours/staff-hours-table.php
@@ -48,42 +48,47 @@
 
   $data = array_merge($metadata, $result);
 
-  //Write data to temporary CSV file on the server
-  $tempfile = fopen('/tmp/DHStempfile.csv', 'w');
-
   foreach ($data as $line) {
-    fputcsv($tempfile, $line);
+    
   }
 
-  fclose($tempfile);
+
+  // //Write data to temporary CSV file on the server
+  // $tempfile = fopen('/tmp/DHStempfile.csv', 'w');
+
+  // foreach ($data as $line) {
+  //   fputcsv($tempfile, $line);
+  // }
+
+  // fclose($tempfile);
 
 
 
-  //Send file to client browser
-  $filename = "/tmp/DHStempfile.csv";
+  // //Send file to client browser
+  // $filename = "/tmp/DHStempfile.csv";
 
-  if(file_exists($filename)){
+  // if(file_exists($filename)){
 
-      //Get file type and set it as Content Type
-      header('Content-Type: application/csv');
+  //     //Get file type and set it as Content Type
+  //     header('Content-Type: application/csv');
 
-      $date = date('Y-m-d');
-      //Use Content-Disposition: attachment to specify the filename
-      header("Content-Disposition: attachment; filename={$tableName}-table-{$date}.csv");
+  //     $date = date('Y-m-d');
+  //     //Use Content-Disposition: attachment to specify the filename
+  //     header("Content-Disposition: attachment; filename={$tableName}-table-{$date}.csv");
 
-      //No cache
-      header('Expires: 0');
-      header('Cache-Control: must-revalidate');
-      header('Pragma: public');
+  //     //No cache
+  //     header('Expires: 0');
+  //     header('Cache-Control: must-revalidate');
+  //     header('Pragma: public');
 
-      //Define file size
-      header('Content-Length: ' . filesize($filename));
+  //     //Define file size
+  //     header('Content-Length: ' . filesize($filename));
 
-      ob_clean();
-      flush();
-      readfile($filename);
-      exit;
-  }
+  //     ob_clean();
+  //     flush();
+  //     readfile($filename);
+  //     exit;
+  // }
 
 ?>
 

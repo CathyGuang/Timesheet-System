c57b3ddd8f6887f323cc24e725f5cf425b466e6f
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-12-16 10:27:56 -0800

    ....

--- staff/full-time/full-time.php
+++ staff/full-time/full-time.php
@@ -25,6 +25,7 @@
     $date = $_POST['Date'];
     $notes = pg_escape_string(trim($_POST['notes']));
     echo $_GET['totalmin'];
+    echo $notes;
     echo $staffID;
 
     if ($_POST['send-email'] == 'true') {

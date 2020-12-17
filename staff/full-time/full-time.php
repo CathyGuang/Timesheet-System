20110da0f23dcd5ffc77456088888c5915390b45
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-12-16 16:17:55 -0800

    kkk

--- staff/full-time/full-time.php
+++ staff/full-time/full-time.php
@@ -20,7 +20,7 @@
 
   <?php
 
-    $staffName = pg_escape_string(trim($_POST['StaffName']));
+    $staffName = trim($_POST['StaffName']);
     $date = $_POST['StaffDate'];
     $staffID = pg_fetch_array(pg_query($db_connection, "SELECT id FROM workers WHERE name = '{$staffName}' AND (archived IS NULL OR archived = '');"), 0, 1)['id'];
     

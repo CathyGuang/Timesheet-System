5a0cf6aef4e12c27e6a39850e4e5df032bec2e03
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-12-17 16:34:05 -0800

    kkk

--- staff/full-time/index.php
+++ staff/full-time/index.php
@@ -351,6 +351,20 @@
       
     
     </form>
+
+    <script type="text/javascript">
+
+      var WORK = <?php $staffShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::STAFF_WORK_TYPE));"));
+      foreach ($staffShiftTypes as $value) { echo "{value:''{$value}'', label:''{$value}''},";}?>;
+
+      console.log(WORK);
+
+
+
+    </script>
+
+
+
           
    
 
@@ -365,6 +379,15 @@
       foreach ($staffNames as $name) {
         $name = htmlspecialchars($name, ENT_QUOTES);
         echo "<option value='{$name}'>";
+      }
+    ?>
+  </datalist>
+
+  <datalist id="work-type-list">
+    <?php
+      $staffShiftTypes = pg_fetch_all_columns(pg_query($db_connection, "SELECT unnest(enum_range(NULL::STAFF_WORK_TYPE));"));
+      foreach ($staffShiftTypes as $value) {
+        echo "{value:''{$value}'', label:''{$value}''},";
       }
     ?>
   </datalist>

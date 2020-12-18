4dcf516fe2ca6a2820626e398133fb3f12b6240d
Lily <lilyli828@DESKTOP-KFGTU6J.localdomain>
2020-12-17 17:20:33 -0800

    kk

--- staff/full-time/js/query.js
+++ staff/full-time/js/query.js
@@ -877,14 +877,29 @@ console.log(worktypearray);
 
 // initiate multiple job select
 var isfirst = true;
-var workInfo=[{value:"brrr",label:"lalala"},
-{value:"brrr",label:"lalala"},{value:"brrr",label:"lalala"},{value:"brrr",label:"lalala"},
-{value:"brrr",label:"lalala"},{value:"brrr",label:"lalala"},{value:"brrr",label:"lalala"},{value:"brrr",label:"lalala"},
-{value:"brrr",label:"lalala"},
-
-
-
-];
+var workInfo=[ 
+    { value: '1', label: 'Admin' },
+    { value: '2', label: 'Riding'},
+    { value: '3', label: 'Riding Admin' },
+    { value: '4', label: 'Vaulting' },
+    { value: '5', label: 'Vaulting Admin' },
+    { value: '6', label: 'Rehab Clinical' },
+    { value: '7', label: 'Rehab Clinical TH' },
+    { value: '8', label: 'Rehab Admin' },
+    { value: '9', label: 'EFP Clinical' },
+    { value: '10', label: 'EFP Clinical TH' },
+    { value: '11', label: 'EFP Supervisor' },
+    { value: '12', label: 'EFP Admin' },
+    { value: '13', label: 'EFL Admin' },
+    { value: '14', label: 'Volunterring Admin' },
+    { value: '15', label: 'Volunteering -barn' },
+    { value: '16', label: 'Vocational Admin' },
+    { value: '17', label: 'Vocational Barn' },
+    { value: '18', label: 'Fundraising' },
+    { value: '19', label: 'Indirect' },
+    { value: '20', label: 'BUILD' },
+    { value: '21', label: 'Major Gift' },
+  ];
 var multipleCancelButton = new Choices(
     '#choices-multiple-remove-button',
     {

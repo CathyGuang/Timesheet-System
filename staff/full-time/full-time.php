cd86523db5d48293829c71c7d0445a48fba597d9
CathyGuang <71342754+CathyGuang@users.noreply.github.com>
2020-12-18 00:13:24 -0600

    h

--- staff/full-time/full-time.php
+++ staff/full-time/full-time.php
@@ -33,7 +33,7 @@
     $inOutTimeArray = explode("\"},{\"", trim($inOutTimeRaw, "[{\"}]"));
     $order1 = array("intime\":","\"","outtime:");
     $replace = '';
-    foreach ($inOutTimeArray as &$line) {
+    foreach ($inOutTimeArray as $line) {
       $line = explode(",", str_replace($order1, $replace, $line));
     }
     
@@ -81,7 +81,7 @@
 
         if ($totalHourResult) {
           echo "<div class='another_shift_title'>Hours recorded successfully.</div>";
-          echo "<form class='submit_and_cancel' action='index.php' method='post'><input name='name' value='{$_POST['selected-name']}' hidden><button type='submit'>Submit another shift</button></form>";
+          echo "<form action='index.php' method='post'><input name='name' value='{$_POST['selected-name']}' hidden><button type='submit'>Submit another shift</button></form>";
           if ($_POST['send-email'] == 'true') {
             $currentDate = date('j-m-Y, g:iA');
             $emailBody = <<<EOT

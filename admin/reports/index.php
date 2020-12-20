88ed729cb13c8eccb725f658288bb42ce0fa21cf
CathyGuang <71342754+CathyGuang@users.noreply.github.com>
2020-12-19 20:33:00 -0600

    h

--- admin/reports/index.php
+++ admin/reports/index.php
@@ -19,10 +19,10 @@
 
   <div class="main-content-div">
 
-    <a href="workers-table.php"><button class="blue-button">Export Staff/Volunteer Data</button></a>
-    <a href="volunteer-hours"><button class="blue-button">Export Volunteer Hours Data</button></a>
-    <a href="staff-hours"><button class="blue-button">Export Staff Hours Data</button></a>
-    <a href="core-staff"><button class="blue-button">Export Core Staff Data</button></a>
+    <a href="workers-table.php"><button class="blue-button">Staff/Volunteer Info</button></a>
+    <a href="volunteer-hours"><button class="blue-button">Volunteer Hours Data</button></a>
+    <a href="staff-hours"><button class="blue-button">Part Time Staff Data</button></a>
+    <a href="core-staff"><button class="blue-button">Full Time Staff Data</button></a>
 
   </div>
 

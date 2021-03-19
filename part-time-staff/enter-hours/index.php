f4ab09e2c2d0c1225d4d135e768b1cd9d6e3d875
Lily <lilyli@eduroam-181-130.dyn.carleton.edu>
2021-03-18 21:55:27 -0500

    kkk

--- part-time-staff/enter-hours/index.php
+++ part-time-staff/enter-hours/index.php
@@ -68,7 +68,7 @@
 
       <div class="form-section">
         <button type="button" class="cancel-form" onclick="window.history.back()">Cancel</button>
-        <button type="button" class="SUB" onclick="submitted();">Submit</button>
+        <button type="button" class="SUB" id="SUB"onclick="submitted();">Submit</button>
       </div>
 
     </form>

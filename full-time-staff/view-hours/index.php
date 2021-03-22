82eb2a4aad9246cde6322d45c921314d834caac4
Lily <lilyli@eduroam-181-130.dyn.carleton.edu>
2021-03-22 12:48:22 -0500

    lkkk

--- full-time-staff/view-hours/index.php
+++ full-time-staff/view-hours/index.php
@@ -3,6 +3,9 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="/static/main.css">
+  <script type="text/javascript" src="/static/jquery.min.js"></script>
+  <script type="text/javascript" src="/static/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/static/enter_hour.css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:700&display=swap" rel="stylesheet">
   <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?>
   <title>View Staff Hours</title>
@@ -19,12 +22,12 @@
 
 
   <div class="form-container">
-    <form autocomplete="off" class="standard-form" action="full-view-hours.php" method="post">
+    <form autocomplete="off"  id = "myform" class="standard-form" action="full-view-hours.php" method="post">
 
       <div class="form-section">
         <div class="form-element">
-          <label for="staff">Name:</label>
-          <input type="text" name="staff" list="staff-list" required>
+          <label for="staff">Name:<div class="name_select_reminder" id="name_select_reminder"></div></label>
+          <input type="text" name="staff" id="selected-name" list="staff-list" required>
         </div>
       </div>
           
@@ -45,7 +48,7 @@
       
       <div class="form-section">
         <button type="button" class="cancel-form" onclick="window.history.back()">Cancel</button>
-        <button type="submit">Go</button>
+        <button type="button" class="purplebut" id="purplebut" onclick="submitted();">Go</button>
       </div>
 
     </form>
@@ -65,5 +68,7 @@
 
 
 </body>
+
+<script type="text/javascript" src="/static/query2.js"></script>
 
 </html>

c36ba4f59859341d96ffc9438f220d92df1f4c0d
Lily <lilyli@LilydeMacBook-Pro.local>
2021-06-23 13:22:24 -0500

    change pay period button

--- full-time-staff/enter-hours/payPeriod.php
+++ full-time-staff/enter-hours/payPeriod.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<meta charset="utf-8">
+  <script src="https://kit.fontawesome.com/22bba2cae7.js" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.js"></script>
+  <script type="text/javascript" src="js/rangeslider.min.js"></script>
+  <script type="text/javascript" src="js/choices.js"></script>
+  <link rel="stylesheet" href="css/slider.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/rangeslider.css">
+  <link rel="stylesheet" href="css/added.css">
+  <link rel="stylesheet" href="css/choices.css">
+  <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?> 
+  <title>Pay Period Check</title>
 </head>
-<body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+<body class= "full-time-report" style="background-color:#D0BDF4;min-width:1226px;background-image: url('hor.png')">
+
+    <div class="full-time-header">
+        <p class="full-time-title">2021 Pay Period</p>
+        <nav class="button-container"> 
+        `<a href="../">`<button class="back-button">Back</button></a>
+        <a href="/"><button class="home-button">Home</button></a>
+        </nav>
+    </div>
+
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
 
 </body>
 </html>

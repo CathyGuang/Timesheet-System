9d2c3e1594a7d078b24a6ab3d6df6b07e77de247
Lily <lilyli@LilydeMacBook-Pro.local>
2021-06-23 15:47:31 -0500

    change pay period button

--- full-time-staff/enter-hours/payPeriod.php
+++ full-time-staff/enter-hours/payPeriod.php
@@ -29,7 +29,7 @@
     <div class= "fulltime-form-container" >
         <form class="white-background" method="post" name="myform" id="myform" action="full-time.php">
             <div class = "pay_table">
-                <table class = "table-fill">
+                <table class = "pay_table_fill">
                 <tr class = "pay_table_row">
                     <th>Start Date</th>
                     <th>End Date</th>

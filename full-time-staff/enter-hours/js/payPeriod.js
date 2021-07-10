1d9b4e3c98d98b973777f1995103dfb417273bd0
Lily <lilyli@LilydeMacBook-Pro.local>
2021-07-09 19:58:07 -0500

    jjj

--- full-time-staff/enter-hours/js/payPeriod.js
+++ full-time-staff/enter-hours/js/payPeriod.js
@@ -1,4 +1,5 @@
 
+// get today's date
 n =  new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
@@ -6,5 +7,22 @@ d = n.getDate();
 document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
 
 
+
+// find next due date
+var timeStampNow = Date.now();
+var timeStampStart = new Date("2021-07-09 00:00:00");
+
+console.log(timeStampNow);
+console.log(timeStampStart);
+for (let i = 0; i < 200; i++) {
+    
+
+}
+
+
+
+
 document.getElementById("dueDate").innerHTML = "dummy Date";
+
+
 

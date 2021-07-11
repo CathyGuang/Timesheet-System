8f6ef1b02a04a99b0ac41079cfc54259c2857e1a
Lily <lilyli@LilydeMacBook-Pro.local>
2021-07-10 20:17:14 -0500

    jjj

--- full-time-staff/enter-hours/payPeriod.php
+++ full-time-staff/enter-hours/payPeriod.php
@@ -45,12 +45,12 @@
                     </div>
                 </div>
             </div>
-            <div class = "light_purple_block">
+            <div class = "purple_block">
                 <div class = "reminder_under_title">
                     Pay Periods and Pay Dates
                 </div>
                 <div class = "pay_table">
-                    
+
                 <table class = "pay_table_fill">
                 <tr class = "pay_table_row">
                     <th>Start Date</th>

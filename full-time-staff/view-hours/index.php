b3307bef17fa6fa53252f1c77d32be850c53a905
Lily <lilyli@LilydeMacBook-Pro.local>
2021-07-11 15:13:10 -0500

    jjj

--- full-time-staff/view-hours/index.php
+++ full-time-staff/view-hours/index.php
@@ -17,7 +17,7 @@
     <h1>Staff Hours</h1>
     <nav> 
       <button onclick="history.back()" >Back</button>
-      <button id = "check_pay_period_button" >Check Pay Periods</button>
+      <button class= "check_pay_period_button" >Check Pay Periods</button>
       <a href="/"><button id="home-button">Home</button></a>
     </nav>
   </header>

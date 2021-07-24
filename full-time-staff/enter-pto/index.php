4092208ec042ec36ffeb556e31830a1717ebb10a
Lily <lilyli@LilydeMacBook-Pro.local>
2021-07-24 02:46:57 -0500

    h

--- full-time-staff/enter-pto/index.php
+++ full-time-staff/enter-pto/index.php
@@ -65,8 +65,8 @@
         
         </div>
         <hr class="line_below_form_inline" style="top:1em;">
-        <div class="time-range-container" style="top:-1em;">
-            <div class = "time-range-title" style="height:45px;">
+        <div class="time-range-container" style="top:-2em;">
+            <div class = "time-range-title" style="height:40px;">
                 Enter Hours
             </div>
             <div class = "enter-hours-pto">

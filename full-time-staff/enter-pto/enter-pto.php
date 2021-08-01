4cd4df0dc7bbdc05bdd2bee0066f2463fe545601
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-01 16:43:25 -0500

    jjj

--- full-time-staff/enter-pto/enter-pto.php
+++ full-time-staff/enter-pto/enter-pto.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
 <link href="https://fonts.googleapis.com/css?family=Nunito:700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="/static/enter_hour.css">
+<link rel="stylesheet" href="/static/main.css">
 <link rel="stylesheet" href="full-time-staff/enter-hours/css/added.css">
 <link rel="stylesheet" href="full-time-staff/enter-hours/css/table.css">
   <?php include $_SERVER['DOCUMENT_ROOT']."/static/scripts/initialization.php"; ?>

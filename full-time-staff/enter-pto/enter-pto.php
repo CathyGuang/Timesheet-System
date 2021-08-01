a67cf60569e79f26aa02f7264ccd11f80ae62728
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-01 17:20:12 -0500

    jjj

--- full-time-staff/enter-pto/enter-pto.php
+++ full-time-staff/enter-pto/enter-pto.php
@@ -81,7 +81,7 @@
 </body>
 <form action='index.php' class= 'submit_another_container'method='post'>
   <button  class='submit_another' type='submit' onclick="window.location.href='index.php'" >Submit Another</button>
-  <button class='submit_another'> Check Pay Period </button>
+  <button class='view-hours'> Check Pay Period </button>
   <button  class='view_hours' type='button' onclick="window.location.href='\../view-hours'" >View My Hours</button>
 </form>
 

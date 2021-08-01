1b9607ff0acf36fb1d4c24dd3d723d4fae4f7447
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-01 17:09:30 -0500

    jjj

--- full-time-staff/enter-pto/enter-pto.php
+++ full-time-staff/enter-pto/enter-pto.php
@@ -36,7 +36,6 @@
     <p class="full-time-title"><?php echo $type?> Hours Successfully Submitted</p>
     <div class="PTO-button-container"> 
       <button class="back-button" onclick="history.back()">Back</button>
-      <button class="view-hours">View Hours</button>
       <button class="home-button" onclick="window.location.href='https://scheduledev.forwardstride.org/'">Home</button>
     </div>
   </div>

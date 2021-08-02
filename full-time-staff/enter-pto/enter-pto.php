aedff8eb29260a64b55453ffb73dcdddb5c9fba1
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-01 19:45:33 -0500

    lll

--- full-time-staff/enter-pto/enter-pto.php
+++ full-time-staff/enter-pto/enter-pto.php
@@ -80,9 +80,9 @@
   </div>
   
 </body>
-<form action='index.php' class= 'submit_another_container'method='post'>
+<form class= 'submit_another_container'method='post'>
   <button  class='submit_another' type='submit' onclick="window.location.href='index.php'" >Submit Another</button>
-  <button  onclick="checkPeriod();"> Check Pay Period </button>
+  <button class='check_pay_period' onclick="checkPeriod();"> Check Pay Period </button>
   <button  class='view_hours' type='button' onclick="window.location.href='\../view-hours'" >View My Hours</button>
 </form>
 

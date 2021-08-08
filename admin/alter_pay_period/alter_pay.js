a90c3f6ab77610b0321d2aaf49539c9be3cbb3c9
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-08 11:03:21 -0500

    jjjj

--- admin/alter_pay_period/alter_pay.js
+++ admin/alter_pay_period/alter_pay.js
@@ -13,15 +13,15 @@ if (typeof(Storage) !== "undefined") {
     localStorage.setItem('myCat', 'Tom');
     var tt = localStorage.getItem('myCat');
     var StartDate = parseInt(localStorage.getItem('startDate'));
-    var EndDate = localStorage.getItem('dueDate');
+    var EndDate = parseInt(localStorage.getItem('dueDate'));
     var control = localStorage.getItem('controll');
 
-    StartDateDate = new Date(StartDate);
-    StartDateDateRead = stampToDate( StartDateDate );
+    StartDateD = new Date(StartDate);
+    EndDateD = new Date(EndDate);
 
 
-    document.getElementById("start-date").innerHTML = stampToDate( StartDateDate );
-    document.getElementById("end-date").innerHTML = new Date(EndDate);
+    document.getElementById("start-date").innerHTML = stampToDate(StartDateD);
+    document.getElementById("end-date").innerHTML = stampToDate(EndDate);
 
     console.log(tt);
     console.log(control);

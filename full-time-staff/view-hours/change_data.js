f525542a4075c17bc7fb6d5c6fef5b058d8c5f29
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-22 17:05:43 -0500

    kkkkk

--- full-time-staff/view-hours/change_data.js
+++ full-time-staff/view-hours/change_data.js
@@ -3,5 +3,8 @@ var splitted_value = changed_value.split("_");
 var value_id = parseInt(splitted_value[0]);
 var value_idD = parseInt(splitted_value[1]);
 
-console.log(splitted_value[0]);
-console.log(splitted_value[1]);
+
+
+
+console.log(value_id);
+console.log(value_idD);

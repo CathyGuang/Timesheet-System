4777c5985e90b11f74130e378cc8fec218f9bcdd
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-22 17:02:14 -0500

    kkkkk

--- full-time-staff/view-hours/change_data.js
+++ full-time-staff/view-hours/change_data.js
@@ -1,4 +1,4 @@
 var changed_value = localStorage.getItem('Change_Value');
-var splitted_value = str.split("_");
+var splitted_value = changed_value.split("_");
 console.log(splitted_value[0]);
 console.log(splitted_value[1]);

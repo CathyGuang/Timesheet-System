007546a0216d8ae9d14d8fe0bfebd2234b16b34a
Lily <lilyli@LilydeMacBook-Pro.local>
2021-08-22 17:03:30 -0500

    kkkkk

--- full-time-staff/view-hours/change_data.js
+++ full-time-staff/view-hours/change_data.js
@@ -1,4 +1,7 @@
 var changed_value = localStorage.getItem('Change_Value');
 var splitted_value = changed_value.split("_");
+var value_id = splitted_value[0];
+var value_idD = splitted_value[1];
+
 console.log(splitted_value[0]);
 console.log(splitted_value[1]);

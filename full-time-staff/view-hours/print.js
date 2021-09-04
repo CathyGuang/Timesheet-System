d9fa82244d06f3037e21e98ca43cc0dd403fc64f
Lily <lilyli@LilydeMacBook-Pro.local>
2021-09-03 23:57:57 -0500

    lkl

--- full-time-staff/view-hours/print.js
+++ full-time-staff/view-hours/print.js
@@ -48,16 +48,16 @@ function genPDF(){
         //rows would be accessed using the "row" variable assigned in the for loop
         for (let j in row.cells) {
           let col = row.cells[j]
-          console.log(col.value);
-        //   if (j == 0){
-        //     console.log("COL:");
-        //     console.log(col.textContent);
-        //   }
-        //   else{
-        //     console.log("EEEE:");
-        //     console.log(col.innerHTML);
+        //   console.log(col);
+          if (j == 0){
+            console.log("COL:");
+            console.log(col.textContent);
+          }
+          else{
+            console.log("EEEE:");
+            console.log(col.children);
 
-        //   }
+          }
           //iterate through columns
           //columns would be accessed using the "col" variable assigned in the for loop
           doc.text("text",20,30+amount); 

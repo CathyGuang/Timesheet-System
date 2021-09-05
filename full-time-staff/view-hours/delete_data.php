d6a6d1dc9d05510e388c78d3aca74862e4635c98
Ziyun-Guang <cathy.guang@newsbreak.com>
2021-09-05 21:27:55 +0200

    revised in out

--- full-time-staff/view-hours/delete_data.php
+++ full-time-staff/view-hours/delete_data.php
@@ -57,7 +57,6 @@
       } else {
         echo "<h3 class='main-content-header'>An error occurred.</h3><p class='main-content-header'>Please try again, ensure that all data is correctly formatted.</p>";
       }
-    }
     ?>
     <!-- UPDATE full_job_hours SET date_of_shift = '2021-08-27', hours = 1 WHERE idd = 9 -->
     

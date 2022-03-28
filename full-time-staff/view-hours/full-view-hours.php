33e53b8cb180c5b5f1d0e3065023b7e3c47e5447
Lily-Li828 <63224318+Lily-Li828@users.noreply.github.com>
2022-03-28 00:19:15 -0500

    jj:

--- full-time-staff/view-hours/full-view-hours.php
+++ full-time-staff/view-hours/full-view-hours.php
@@ -125,6 +125,7 @@
     $inOutData = pg_fetch_all(pg_query($db_connection, $inOutQuery));
     $holidayData = pg_fetch_all(pg_query($db_connection, $holidayQuery));
 
+    echo $coreData;
     if (!$coreData) {
         echo "<h3 class='main-content-header'>No data.</h3><p class='main-content-header'>There are no hour entries for this time period.</p>";
         return;
